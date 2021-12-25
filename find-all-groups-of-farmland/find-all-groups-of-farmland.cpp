4d8a6b555335db663c2ebf24fdba4d22082ea89d
AMAN BARNWAL <aman25barnwal@gmail.com>
2021-12-25 18:57:54 +0530

    Time: 152 ms (54.90%), Space: 70.7 MB (32.84%) - LeetHub

--- find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,59 +1,28 @@
 class Solution {
 public:
     
+    // Using DFS on 2D Grid
+    
     // All 4 directions : up, right, down, left
     // (-1, 0), (0, 1), (1, 0), (0, -1)
     
     int dx[4] = {-1, 0, 1,  0};
     int dy[4] = { 0, 1, 0, -1};
     
-    bool isValid(int x, int y, vector<vector<int>> &vis, vector<vector<int>> &grid, int n, int m) {
+    void dfs(int x, int y, int n, int m, vector<vector<int>> &vis, vector<vector<int>> &grid, int &x_max, int &y_max) {
         
-        if(x >= 0 && x < n && y >= 0 && y < m && vis[x][y] == 0 && grid[x][y] == 1)         return true;
-        return false;
-    }
-    
-    void bfs(int x, int y, int n, int m, vector<vector<int>> &vis, vector<vector<int>> &grid, vector<vector<int>> &res) {
+        // Base Condition
+        if(x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0 || vis[x][y] == 1)         return;
         
-        int x_min = n-1, y_min = m-1;
-        int x_max = 0, y_max = 0;
-        
-        vector<int> path;
-        queue<pair<int, int>> q;
-        
-        q.push({x, y});
+        // Make the current node visited
         vis[x][y] = 1;
         
-        while(q.empty() == false) {
-            
-            pair<int, int> temp = q.front();
-            q.pop();
-
-            x_min = min(x_min, temp.first);
-            y_min = min(y_min, temp.second);
-            
-            x_max = max(x_max, temp.first);
-            y_max = max(y_max, temp.second);
-            
-            for(int i=0; i<4; i++) {
-              
-                int wX = temp.first + dx[i];
-                int wY = temp.second + dy[i];
-                
-                if(isValid(wX, wY, vis, grid, n, m) == true) {
-                    q.push({wX, wY});
-                    vis[wX][wY] = 1;
-                }
-            }
+        x_max = max(x_max, x);
+        y_max = max(y_max, y);
+        
+        for(int i=0; i<4; i++) {
+            dfs(x + dx[i], y + dy[i], n, m, vis, grid, x_max, y_max);
         }
-        
-        path.push_back(x_min);
-        path.push_back(y_min);
-        path.push_back(x_max);
-        path.push_back(y_max);
-        
-        res.push_back(path);
-        path.clear();
         
         return;
     }
@@ -66,12 +35,30 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
+        int x_min, y_min, x_max, y_max;
+        
         vector<vector<int>> vis(n, vector<int>(m, 0));
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
+                
                 if(grid[i][j] == 1 && vis[i][j] == 0) {
-                    bfs(i, j, n, m, vis, grid, res);
+                   
+                    x_min = i;
+                    y_min = j;
+                    
+                    x_max = i;
+                    y_max = j;
+                    
+                    dfs(i, j, n, m, vis, grid, x_max, y_max);
+
+                    path.push_back(x_min);
+                    path.push_back(y_min);
+                    path.push_back(x_max);
+                    path.push_back(y_max);
+
+                    res.push_back(path);
+                    path.clear();
                 }
             }
         }
