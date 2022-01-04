f1515e4bca3e32ab6bc9507a0302370534ace8a8
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-05 02:26:05 +0530

    Time: 8 ms (60.81%), Space: 11.8 MB (5.78%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -11,55 +11,43 @@
 class Solution {
 public:
     
-    void addNodeAtBeginning(ListNode** head, int val) {
-        
-        ListNode* temp = new ListNode(val);
-        
-        if(head == NULL) {
-            *head = temp;
-            return;
-        }
-        
-        else {
-            temp->next = *head;
-            *head = temp;
-        }
-        
-        return;
-    }
+   
     
     ListNode* rotateRight(ListNode* head, int k) {
     
-        if(head == NULL)                                        return NULL;
-        
-        vector<int> v;
+        if (head == NULL || head->next == NULL)                     return head;
         
         ListNode* temp = head;
         
-        while(temp != NULL) {
-            v.push_back(temp->val);
+        int len = 0;
+        ListNode* prev = NULL;
+        
+        while (temp != NULL) {
+            
+            prev = temp;
+            temp = temp->next;
+            
+            len++;
+        }
+
+        // Close the list (create a ring)
+        temp = prev;
+        
+        temp->next = head;
+        temp = head;
+        
+        
+        // Find one element before the new head
+        for(int i=0; i<len - (k % len) - 1; i++) {
             temp = temp->next;
         }
         
-        int n = v.size();
+        head = temp->next;
         
-        k = k % n;
-        vector<int> v1(n);
+        // Open the list (break the ring)
+        temp->next = NULL;
         
-        // Rotate v to the right by k places
-        for(int i=0; i<n; i++) {
-            v1[(i+k+n) % n] = v[i];
-        }        
-        
-        reverse(v1.begin(), v1.end());
-        
-        ListNode* res = NULL;
-        
-        for(auto &x : v1) {
-           addNodeAtBeginning(&res, x);
-        }
-        
-        return res;
+        return head;
     }
     
 };
