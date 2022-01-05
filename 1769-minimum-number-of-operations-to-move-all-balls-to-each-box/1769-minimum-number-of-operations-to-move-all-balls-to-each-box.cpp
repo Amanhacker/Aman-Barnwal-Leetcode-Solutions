c0f987c3a354bdb6e2dd557c35dbf03b59cd01d7
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-06 02:17:44 +0530

    Time: 12 ms (60.28%), Space: 9.9 MB (5.41%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -6,19 +6,26 @@ public:
         vector<int> res;
         int n = boxes.length();
         
-        for(int i=0; i<n; i++) {
-            
-            int sum = 0;
-            
-            for(int j=0; j<n; j++) {
-                if(j != i) {
-                    if(boxes[j] == '1')                           sum += abs(j - i);
-                }
-            }
-            
-            res.push_back(sum);
+        vector<int> left(n), right(n);
+        
+        left[0] = 0;
+        right[n-1] = 0;
+        
+        int noOfBalls = 0;
+        
+        for(int i=1; i<n; i++) {
+            if(boxes[i-1] == '1')                               noOfBalls++;
+            left[i] = left[i-1] + noOfBalls;
         }
         
+        noOfBalls = 0;
+        
+        for(int i=n-2; i>=0; i--) {
+            if(boxes[i+1] == '1')                               noOfBalls++;
+            right[i] = right[i+1] + noOfBalls;
+        }
+        
+        for(int i=0; i<n; i++)                                  res.push_back(left[i] + right[i]);
         return res;
     }
     
