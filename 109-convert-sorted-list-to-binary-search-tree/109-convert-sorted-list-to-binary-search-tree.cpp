4a26b42e03ede6ee2373f0692c451d7fa6b6ba81
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-05 12:18:42 +0530

    Time: 656 ms (5.42%), Space: 359.4 MB (5.07%) - LeetHub

---
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   
+    TreeNode* convertBST(vector<int> v, int start, int end) {
+        
+        TreeNode* root = NULL;
+        
+        if(start > end)                                                    return NULL;
+        
+        int mid = start + (end - start) / 2;
+        
+        root = new TreeNode(v[mid]);
+        
+        root->left = convertBST(v, start, mid - 1);
+        root->right = convertBST(v, mid + 1, end);
+        
+        return root;
+    }
+    
+    TreeNode* sortedListToBST(ListNode* head) {
+    
+        TreeNode* res = NULL;
+        
+        if(head == NULL)                                                    return res;
+        
+        vector<int> v;
+        ListNode* temp = head;
+        
+        while(temp != NULL) {
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+        
+        // v contains element in ascending order
+        
+        // Convert it into height balanced BST
+        
+        int mid = v.size() / 2;
+        int n = v.size();
+        
+        TreeNode* ans = convertBST(v, 0, n-1);
+        
+        return ans;
+    }
+    
+};
