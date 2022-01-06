e0b4136746583fad3a4b398c47c6c655b7d2d3cc
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-07 02:45:39 +0530

    Time: 62 ms (17.62%), Space: 19.8 MB (61.55%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,33 @@
+class KthLargest {
+public:
+    
+    // Form max heap
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int size;
+    
+    KthLargest(int k, vector<int>& v) {
+        
+        for(auto &x : v) {
+            pq.push(x);
+            if(pq.size() > k)                    pq.pop();
+        }         
+        
+        size = k;
+    }
+    
+    int add(int val) {
+        
+        pq.push(val);
+        
+        if(pq.size() > size)                     pq.pop();
+        
+        return pq.top();
+    }
+    
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
