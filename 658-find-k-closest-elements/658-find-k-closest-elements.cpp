33e45ba2cc12db68f838676be3b2936ceef5c719
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-08 21:26:47 +0530

    Time: 67 ms (40.77%), Space: 31.7 MB (56.97%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     
-    int x1;
- 
-//     static bool cmp(const int &a, const int &b) {
-        
-//         if(abs(a - x1) == abs(b - x1))                      return a < b;
-//         else                                                return abs(a - x1) < abs(b - x1);
-//     }
     
     vector<int> findClosestElements(vector<int>& v, int k, int x) {
     
         vector<int> res;
         
-        x1 = x;
-        
         // Sort the elements of v according to distance from x
-        //sort(v.begin(), v.end(), cmp);
         
         stable_sort(v.begin(), v.end(), [x](const auto a, const auto b) {
             return abs(a - x) < abs(b - x);  
