824fff54d3cca800685be00728489b508afba7eb
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-10 00:41:37 +0530

    Time: 4 ms (17.39%), Space: 6.2 MB (53.45%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    bool checkValidString(string s) {
+    
+        int n = s.length();
+        stack<int> open, star;
+        
+        for(int i=0; s[i]!='\0'; i++) {
+            
+            if(s[i] == '(')                                         open.push(i);
+            else if(s[i] == '*')                                    star.push(i);
+            else {
+                
+                if(open.empty() == false)                           open.pop();
+                else if(star.empty() == false)                      star.pop();
+                else                                                return false;
+            }
+        }
+        
+        // Now process leftover opening brackets
+        while(open.empty() == false) {
+            
+            if(star.empty() == true)                                return false;
+            
+            else if(open.top() < star.top()) {
+                
+                open.pop();
+                star.pop();
+            }
+            
+            // CASE: open.top() > star.top()
+            
+            else                                                    return false;
+        }
+        
+        return true;
+    }
+    
+};
