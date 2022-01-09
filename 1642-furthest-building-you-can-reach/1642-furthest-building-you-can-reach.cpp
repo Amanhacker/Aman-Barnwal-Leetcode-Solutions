ede1bb7b109e27c1f05b79c3ce110a1f9648a5ec
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-09 17:06:23 +0530

    Time: 194 ms (15.48%), Space: 69.6 MB (12.87%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
     
-    // Using Min Heap
+    // Using Multiset
     
     int furthestBuilding(vector<int>& h, int bricks, int ladders) {
     
         int n = h.size();
         
-        // Using Min Heap
-        priority_queue<int, vector<int>, greater<int>> pq;
-        
+        multiset<int> s;
         int i = 0;
         
-        while(i < n - 1 && pq.size() < ladders) {
+        for( ; i<n-1; i++) {
             
             int jumpHeight = h[i + 1] - h[i];
+            if(jumpHeight < 0)                          continue;
             
-            if(jumpHeight > 0)                              pq.push(jumpHeight);
-            i++;
-        }
-        
-        while(i < n - 1) {
+            s.insert(jumpHeight);
             
-            int jumpHeight = h[i + 1] - h[i];
-            
-            if(jumpHeight > 0) {
-               
-                if(pq.empty() == false && jumpHeight > pq.top()) {
+            if(s.size() > ladders) {
                 
-                    // We here found a larger current jump, so use ladder for this
-                    bricks -= pq.top();
-                    pq.pop();
-
-                    pq.push(jumpHeight);
-                }
-
-                else                                        bricks -= jumpHeight;
+                bricks -= *s.begin();
+                s.erase(s.begin());
             }
             
-            if(bricks < 0)                                  return i;
-            i++;
+            if(bricks < 0)                              return i;
         }
         
         return i;
