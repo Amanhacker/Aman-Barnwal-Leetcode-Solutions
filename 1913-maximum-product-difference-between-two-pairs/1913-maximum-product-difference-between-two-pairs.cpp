c635638c4d67d13b62b54dbccb4e352f0337c8be
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-10 23:38:57 +0530

    Time: 24 ms (90.10%), Space: 20.4 MB (14.96%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -6,9 +6,31 @@ public:
         int res;
         int n = a.size();
         
-        sort(a.begin(), a.end());
+        int max1 = INT_MIN, max2 = INT_MIN;
+        int min1 = INT_MAX, min2 = INT_MAX;
         
-        res = (a[n-1] * a[n-2]) - (a[0] * a[1]);
+        for(int i=0; i<n; i++) {
+            
+            if(a[i] > max1) {
+                max2 = max1;
+                max1 = a[i];
+            }
+            
+            else if(a[i] > max2) {
+                max2 = a[i];
+            }
+            
+            if(a[i] < min1) {
+                min2 = min1;
+                min1 = a[i];
+            }
+            
+            else if(a[i] < min2) {
+                min2 = a[i];
+            }
+        }
+            
+        res = (max2 * max1) - (min2 * min1);
         return res;
     }
     
