165b0ce2aba9445eef58087bbe09c2be03d401cc
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 02:35:30 +0530

    Time: 36 ms (16.32%), Space: 16.9 MB (6.14%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    static bool cmp(const pair<int, string> &a, const pair<int, string> &b) {
+        return a.first < b.first;
+    }
+    
+    string kthDistinct(vector<string>& v, int k) {
+    
+        string res = "";
+        
+        // d contains distinct string in the same order         // 1st arg - index, 2nd arg - string
+        vector<pair<int, string>> d; 
+        int n = v.size();
+        
+        unordered_map<string, pair<int, int>> u;               // 1st arg - string, 2nd arg - index, 3rd arg - freq
+        
+        for(int i=0; i<n; i++) {            
+            u[v[i]].second++;
+            u[v[i]].first = i;
+        }
+        
+        for(auto &x : u) {
+            if(x.second.second == 1)                            d.push_back({x.second.first, x.first});
+        }
+        
+        sort(d.begin(), d.end(), cmp);
+        if(d.size() < k)                                        return res;
+        
+        res = d[k-1].second;
+        return res;
+    }
+    
+};
