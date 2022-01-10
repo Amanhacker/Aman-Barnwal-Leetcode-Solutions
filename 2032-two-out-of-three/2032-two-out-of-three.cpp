77dd8e66e74d6dc9481eac20ef81146e30ff4eaa
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 03:11:44 +0530

    Time: 12 ms (89.45%), Space: 25.3 MB (92.90%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -4,14 +4,14 @@ public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
     
         vector<int> res;
-        bool cnt[3][101] = {};
+        int cnt[3][101] = {};
         
-        for(auto n : nums1)                                         cnt[0][n] = true;
-        for(auto n : nums2)                                         cnt[1][n] = true;
-        for(auto n : nums3)                                         cnt[2][n] = true;
+        for(auto n : nums1)                                         cnt[0][n] = 1;
+        for(auto n : nums2)                                         cnt[1][n] = 1;
+        for(auto n : nums3)                                         cnt[2][n] = 1;
         
         for (int i=1; i<=100; i++) {
-            if(cnt[0][i] + cnt[1][i] + cnt[2][i] > 1)               res.push_back(i);
+            if(cnt[0][i] + cnt[1][i] + cnt[2][i] >= 2)              res.push_back(i);
         }
         
         return res;
