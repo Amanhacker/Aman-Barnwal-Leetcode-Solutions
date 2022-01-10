eb68efc57fbb451c06bafc50fabc660fb5dd6718
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 04:17:37 +0530

    Time: 2 ms (26.62%), Space: 6.3 MB (70.33%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -2,31 +2,23 @@ class Solution {
 public:
     
     bool areNumbersAscending(string s) {
-    
-        int n = s.length();
-        vector<int> res;
         
-        for(int i=0; i<n; i++) {
+        int prev = 0, cur = 0;
+        
+        for (auto ch : s) {
             
-            if(isdigit(s[i]) == true) {
-                // Push the digit into res
+            if(isdigit(ch) == true)                    cur = cur * 10 + (ch - '0');
+            
+            else if(cur != 0) {
                 
-                string temp = "";
-                
-                while(s[i] != ' ' && i < n) {
-                    temp += s[i];
-                    i++;
-                }
-                
-                res.push_back(stoi(temp));
+                if (prev >= cur)                       return false;
+            
+                prev = cur;
+                cur = 0;
             }
         }
         
-        for(int i=1; i<res.size(); i++) {
-            if(res[i-1] >= res[i])                          return false;
-        }
-        
-        return true;
+        return (cur == 0 || prev < cur);
     }
     
 };
