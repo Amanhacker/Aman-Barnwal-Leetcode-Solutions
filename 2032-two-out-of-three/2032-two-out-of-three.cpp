ee9c1b135ffc67927b0b238716dae6623ac85fb8
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 03:11:11 +0530

    Time: 20 ms (61.14%), Space: 25.3 MB (82.94%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -4,31 +4,14 @@ public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
     
         vector<int> res;
-        set<int> w;
+        bool cnt[3][101] = {};
         
-        unordered_map<int, int> u;              // 1st arg - element, 2nd arg - No.of arrays in which this element is present
+        for(auto n : nums1)                                         cnt[0][n] = true;
+        for(auto n : nums2)                                         cnt[1][n] = true;
+        for(auto n : nums3)                                         cnt[2][n] = true;
         
-        for(auto &x : nums1) {
-            if(w.find(x) == w.end())            u[x]++;
-            w.insert(x);
-        }
-        
-        w.clear();
-        
-        for(auto &x : nums2) {
-            if(w.find(x) == w.end())            u[x]++;
-            w.insert(x);
-        }
-        
-        w.clear();
-        
-        for(auto &x : nums3) {
-            if(w.find(x) == w.end())            u[x]++;
-            w.insert(x);
-        }
-        
-        for(auto &x : u) {
-            if(x.second >= 2)                   res.push_back(x.first);
+        for (int i=1; i<=100; i++) {
+            if(cnt[0][i] + cnt[1][i] + cnt[2][i] > 1)               res.push_back(i);
         }
         
         return res;
