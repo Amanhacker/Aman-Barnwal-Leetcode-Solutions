8cc9a34f1d77b976d77b3bb26bde6dbcf4492f67
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 01:40:46 +0530

    Time: 0 ms (100.00%), Space: 6 MB (28.78%) - LeetHub

---
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int sumBase(int n, int k) {
+    
+        int sum = 0;
+        int temp = n;
+        
+        vector<int> digits;
+        
+        // First, convert the number n from base 10 to base k
+        while(n > 0) {
+            
+            int dig = n % k;
+            digits.push_back(dig);
+            
+            n =  n / k;
+        }        
+        
+        for(auto &x : digits)               sum += x;
+        
+        return sum;
+    }
+    
+};
