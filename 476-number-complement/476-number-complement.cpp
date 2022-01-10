3655e4193319a935df80cbec2179f45e84beaf3b
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 05:15:01 +0530

    Time: 4 ms (5.37%), Space: 5.9 MB (78.98%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -3,28 +3,10 @@ public:
     
     int findComplement(int n) {
         
-        string res = "";
+        unsigned mask = ~0;
+        while(mask & n)                                 mask = mask << 1;
         
-        while(n > 0) {
-            res += to_string(n % 2);
-            n = n / 2;
-        }
-        
-        for(int i=0; i<res.size(); i++) {
-            if(res[i] == '0')                           res[i] = '1';
-            else                                        res[i] = '0';
-        }
-        
-        // Here, res contains binary in reverse order
-        int ans = 0, mul = 1;;
-        
-        
-        for(int i=0; i<res.size(); i++) {
-            ans += ((int)(res[i]) - '0') * mul;
-            if(mul < INT_MAX / 2)                             mul *= 2;
-        }
-        
-        return ans;
+        return ~n ^ mask;
     }
     
 };
