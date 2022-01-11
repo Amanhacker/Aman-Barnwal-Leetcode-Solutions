a6d0c28a2eaee20047b1808aeeab9380e2ad5156
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 17:02:39 +0530

    Time: 4 ms (56.82%), Space: 6.3 MB (31.59%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    int getLucky(string s, int k) {
+    
+        int n = s.length();
+        string temp = "";
+        
+        // Replace the s[i] with its position in the alphabet, and then its sum
+        for(int i=0; i<n; i++) {
+            
+            int t = (int)(s[i]) - 97 + 1;
+            temp += (to_string)(t);
+        }
+        
+        int sum;
+        
+        while(k--) {
+            
+            sum = 0;
+            
+            for(int i=0; i<temp.size(); i++) {
+                sum += (int)(temp[i]) - 48;   
+            }
+            
+            temp = to_string(sum);
+        }
+        
+        return sum;
+    }
+    
+};
