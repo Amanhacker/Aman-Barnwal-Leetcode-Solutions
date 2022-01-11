0179d043cc0b625cd6ded820d496d56a67642041
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-12 01:10:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.52%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -5,20 +5,9 @@ public:
     
         int count = 0;
         
-        if(low % 2 == 1 && high % 2 == 1) {
-            count += ( (high - 1) - (low + 1) ) / 2;
-            count += 2;
-        }   
-        
-        else if(low % 2 == 0 && high % 2 == 0) {
-            count += (high - low) / 2;
-        }
-        
-        // Any one of them is odd
-        else {
-            count += 1;
-            count += (high - low) / 2;
-        }
+        count += (high - low) / 2;
+
+        if(low % 2 == 1 || high % 2 == 1)                   count++;
         
         return count;
     }
