ac1266aca0d9abd12538b1d9e8f967a185e6506c
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 14:30:37 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (23.94%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -3,32 +3,24 @@ public:
     
     int findMiddleIndex(vector<int>& a) {
     
-        int ans = INT_MAX;
         int n = a.size();
         
+        vector<int> left(n), right(n);
+        
+        // left[] is the cumulative sum from left to right
+        // right[] is the cumulative sum from right to left
+        
+        left[0] = a[0];
+        for(int i=1; i<n; i++)                          left[i] = a[i] + left[i-1];
+        
+        right[n-1] = a[n-1];
+        for(int i=n-2; i>=0; i--)                       right[i] = a[i] + right[i+1];
         
         for(int i=0; i<n; i++) {
-    
-            // Taking i as middleIndex
-            int leftSum = 0, rightSum = 0;
-            
-            // Calculate leftSum
-            if(i == 0)                                  leftSum = 0;
-            else {
-                for(int k=0; k<i; k++)                  leftSum += a[k];
-            }
-            
-            // Calculate rightSum
-            if(i == n-1)                                rightSum = 0;
-            else{
-                for(int k=i+1; k<n; k++)                rightSum += a[k];
-            }
-                    
-            if(leftSum == rightSum)                     ans = min(ans, i);                
+            if(left[i] == right[i])                     return i;
         }
         
-        if(ans != INT_MAX)                              return ans;
-        else                                            return -1;
+        return -1;
     }
     
 };
