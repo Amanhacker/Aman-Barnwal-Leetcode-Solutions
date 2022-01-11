ad14b382fd1a2ce31ed9e61045d427cd5625a395
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 20:14:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.48%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -7,26 +7,20 @@ public:
         
         // Zeroes in left, and ones in right
         int n = s.length();
+        int rightOnes = 0, leftZeroes = 0;
         
-        vector<int> left(n), right(n);
-        
-        if(s[0] == '0')               left[0] = 1;
-        else                          left[0] = 0;
-        
-        for(int i=1; i<n; i++) {
-            if(s[i] == '0')           left[i] = left[i-1] + 1;
-            else                      left[i] = left[i-1];
+        for(auto &x: s) {
+            if(x == '1')                            rightOnes++;
         }
         
-        if(s[n-1] == '1')             right[n-1] = 1;
-        else                          right[n-1] = 0;
-        
-        for(int i=n-2; i>=0; i--) {
-            if(s[i] == '1')           right[i] = right[i+1] + 1;
-            else                      right[i] = right[i+1];
-        }
-        
-        for(int i=0; i<n-1; i++)      res = max(res, left[i] + right[i+1]);
+        for(int i=0; i<n-1; i++) {
+            
+            if(s[i] == '0')                         leftZeroes++;
+            else                                    rightOnes--;
+            
+            res = max(res, leftZeroes + rightOnes);
+        }   
+
         return res;
     }
     
