16c781bb3a88f125c5792a814aa9e1fda592719b
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 19:37:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (49.86%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -6,7 +6,8 @@ public:
         int maxi = -1;
         vector<int> v(26, -1);
         
-        for (int i = 0; i < s.size(); i++) {
+        for (int i=0; i<s.size(); i++) {
+            
             if(v[s[i] - 'a'] == -1)                            v[s[i] - 'a'] = i;
             else                                               maxi = max(maxi, abs(v[s[i] - 'a'] - i) - 1);      
         }
