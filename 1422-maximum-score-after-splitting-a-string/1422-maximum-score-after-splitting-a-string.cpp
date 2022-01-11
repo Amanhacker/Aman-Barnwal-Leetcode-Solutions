f7807d9451c8b9ef4f9514dd065b6cd4bd069e61
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 20:11:38 +0530

    Time: 8 ms (19.34%), Space: 6.6 MB (14.96%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -14,33 +14,19 @@ public:
         else                          left[0] = 0;
         
         for(int i=1; i<n; i++) {
-            
             if(s[i] == '0')           left[i] = left[i-1] + 1;
             else                      left[i] = left[i-1];
-            
-            cout << left[i] << " ";
         }
         
         if(s[n-1] == '1')             right[n-1] = 1;
         else                          right[n-1] = 0;
         
-        cout << endl;
-        
         for(int i=n-2; i>=0; i--) {
-            
             if(s[i] == '1')           right[i] = right[i+1] + 1;
             else                      right[i] = right[i+1];
-            
-            cout << right[i] << " ";
         }
         
-        cout << "AMan" << endl;
-        
-        for(int i=0; i<n-1; i++) {
-            cout << left[i] << " " << right[i] << endl;
-            res = max(res, left[i] + right[i+1]);
-        }
-        
+        for(int i=0; i<n-1; i++)      res = max(res, left[i] + right[i+1]);
         return res;
     }
     
