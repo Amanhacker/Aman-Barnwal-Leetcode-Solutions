b43bab3c6c6b28d7f42897e2d7754b4ba51e333f
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-11 15:21:05 +0530

    Time: 6 ms (7.29%), Space: 8.3 MB (83.54%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cpp
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cpp
@@ -3,23 +3,16 @@ public:
     
     int maxAscendingSum(vector<int>& a) {
     
-        int ans = 0;
+        int ans = a[0];
         int n = a.size();
         
+        int sum = a[0];
+        
         // Find all ascending subarray
-        for(int i=0; i<n; i++) {
+        for(int i=1; i<n; i++) {
             
-            // Subarray starts at index i
-            int k = i, sum = a[i];
-            
-            while(k < n-1) {
-                
-                if(a[k] < a[k+1]) {
-                    sum += a[k+1];
-                    k++;
-                }    
-                else                                break;
-            }
+            if(a[i-1] < a[i])                           sum += a[i];
+            else                                        sum = a[i];
             
             ans = max(ans, sum);
         }
