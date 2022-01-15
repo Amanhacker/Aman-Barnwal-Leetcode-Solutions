a1e396dfda7f1b9d62f69eab585165bf84e96643
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-15 18:59:49 +0530

    Time: 27 ms (39.85%), Space: 5.8 MB (94.36%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+     bool isLeap(int y) {
+        
+        // A year is a leap year iff 
+        //        1). year % 400 == 0
+        //        2). year % 4 == 0 && year % 100 != 0
+        
+        return y % 4 == 0 && (y % 100 != 0 || y % 400 == 0); 
+    }
+    
+    int dayOfYear(string date) {
+    
+        int res = 0;
+        int days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        
+        int m = (stoi)(date.substr(5, 2));
+        int d = (stoi)(date.substr(8));
+        int y = (stoi)(date.substr(0, 4));
+        
+        res += d;
+        
+        if(m > 2 && isLeap(y) == true)                          res++;
+        
+        for(int i=0; i<=m-2; i++)                               res += days[i];
+        
+        return res;
+    }
+    
+};
