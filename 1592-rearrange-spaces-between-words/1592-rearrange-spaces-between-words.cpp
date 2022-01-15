2236b48f2d8dbb412990c3948521399e3caac481
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-15 20:58:45 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (48.90%) - LeetHub

---
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    
+    string reorderSpaces(string text) {
+    
+        string res = "";
+        
+        int noSpaces = 0;
+        vector<string> words;
+        
+        string temp = "";
+        
+        for(int i=0; i<text.length(); ) {
+            
+            if(text[i] == ' ') {
+
+                while(i < text.length() && text[i] == ' ') {
+                    i++;
+                    noSpaces++;
+                }
+                
+                if(temp != "")                              words.push_back(temp);
+                temp = "";
+            }
+            
+            else {
+                temp += text[i];
+                i++;
+            }
+        }
+        
+        if(temp != "")                                      words.push_back(temp);
+        
+        int noWords = words.size();
+        
+        for(auto &x : words)                                cout << x << " ";
+        cout << endl;
+        
+     
+        
+        cout << noWords << endl;
+        
+        if(noWords == 1) {
+            res += words[0];
+            
+            int c = noSpaces;
+            while(c--)                                      res += ' ';
+            
+            return res;
+        }
+        
+        int spacesExtra, spacesBetweenWords;
+        
+        spacesExtra = noSpaces % (noWords - 1);
+
+        if(spacesExtra == 0) {
+            // Then we can put these spaces between each word  
+            spacesBetweenWords = noSpaces / (noWords - 1);
+        } 
+        
+        cout << spacesExtra << endl;
+        cout << spacesBetweenWords << endl;
+        
+        // Else put extra spaces at the last
+        
+        for(int i=0; i<words.size(); i++) {
+            
+            res += words[i];
+
+            if(i == words.size() - 1)                   break;
+
+            int c = spacesBetweenWords;
+            while(c--)                                  res += ' ';
+        }
+        
+        if(spacesExtra > 0) {
+            int c = spacesExtra;
+            while(c--)                                  res += ' ';
+        }
+        
+        return res;
+    }
+    
+};
