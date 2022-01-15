bfa66b17f7c74d35424a851280852b4e02606ea0
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-15 22:00:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.61%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    char findRequired(int ind, string time) {
+        
+        if(ind == 0) {
+            if(time[1] == '?' || time[1] <= '3')          return '2';
+            else                        return '1';
+        }   
+        
+        else if(ind == 1) {
+           if(time[0] <= '1')           return '9';
+            else                        return '3';
+        }
+        
+        else if(ind == 3)               return '5';
+        else if(ind == 4)               return '9';
+        
+        return '0';
+    } 
+    
+    string maximumTime(string time) {
+    
+        // Time would be between 00:00 and 23:59
+        
+        // 1st digit - [0, 2],          i.e     i = 0
+        
+        // i.e  i = 1
+        
+        // 2nd digit - [0, 9], if 1st digit = [0, 1]
+        // 2nd digit - [0, 3], if 1st digit = '2'
+        
+        // 3rd digit - [0, 5]           i.e     i = 3
+        // 4th digit - [0, 9]           i.e     i = 4
+        
+        for(int i=0; i<time.length(); i++) {
+            if(time[i] == '?')                      time[i] = findRequired(i, time);
+        }
+        
+        return time;
+    }
+    
+};
