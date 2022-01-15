8276de070aa86b4fe1895f09d33e92cfaca70881
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-15 21:25:56 +0530

    Time: 22 ms (55.02%), Space: 13.5 MB (54.25%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -5,6 +5,8 @@ public:
     
         int n = s.length();
         
+        // On removing the first and the last characters, if there exists some pattern, we would still be able to find the original string somewhere in the middle, taking some characters from the first half and some from the second half.
+        
         return (s + s).substr(1, 2*n-2).find(s) != -1;
     }
     
