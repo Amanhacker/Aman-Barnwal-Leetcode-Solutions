751ef920fa765a7df6a95046b0a845a7f1439764
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-15 23:46:59 +0530

    Time: 30 ms (6.04%), Space: 6.4 MB (15.38%) - LeetHub

---
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    bool isLongPressedName(string name, string typed) {
+    
+        int n = name.length();
+        int m = typed.length();
+        
+        for(int i=0, j=0; i < n && j < m; ) {
+            
+            if(name[i] == typed[j]) {
+
+                cout << i << " " << j << endl;
+                
+                int countInName = 1, countInTyped = 1;
+                char ch = name[i];
+                
+                while(i + 1 < n && name[i] == name[i+1]) {
+                    countInName++;
+                    i++;
+                }
+                
+                while(j + 1 < m && typed[j] == typed[j+1]) {
+                    countInTyped++;
+                    j++;
+                }
+                
+                i++;
+                j++;
+                
+                cout << ch << " " << countInName << " " << countInTyped << endl;
+                
+                if(countInName > countInTyped)                                  return false;
+                
+                if(i == n && j < m)                                             return false;
+                if(i < n && j == m)                                             return false;
+            }
+            
+            else                                                                return false;
+        }
+        
+        return true;
+    }
+    
+};
