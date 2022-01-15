1723c055a3f609e1024c05029a2dd4aecaab1b08
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-15 21:43:46 +0530

    Time: 7 ms (84.43%), Space: 9.2 MB (6.12%) - LeetHub

---
+++ 482-license-key-formatting/482-license-key-formatting.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    
+    string licenseKeyFormatting(string s, int k) {
+    
+        string res = "";
+        
+        int n = s.length();
+        
+        int noDashes = count(s.begin(), s.end(), '-');
+        int noLetters = n - noDashes;
+        
+        if(noLetters == 0)                                      return "";
+        if(n < k)                                               return s;
+        
+        string temp = "";
+        
+        for(auto &x : s) {
+            if(x != '-')                                        temp += toupper(x);
+        }
+        
+        int noGroups, firstGroupNo = 0;
+        
+        if(noLetters % k != 0) {
+            // First group is having no.os letters < k
+            firstGroupNo = noLetters % k;
+        }
+        
+        noGroups = noLetters / k;
+        int i = 0;
+        
+        if(firstGroupNo > 0) {
+            
+            int c = firstGroupNo;
+            res += temp.substr(i, c);
+            
+            i = c;
+            
+            res += '-';
+        }
+        
+        while(i < temp.size()) {
+            
+            if(i + k - 1 < temp.size())                        res += temp.substr(i, k);
+            
+            if(i + k == temp.size())                           break;
+            
+            res += '-';
+            i = i + k;
+        }
+        
+        return res;
+    }
+    
+};
