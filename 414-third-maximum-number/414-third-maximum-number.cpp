9644a6237ae25a991cfd85009bd1dd63c717e6d9
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-16 00:42:02 +0530

    Time: 7 ms (61.21%), Space: 9.1 MB (44.29%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -3,18 +3,36 @@ public:
     
     int thirdMax(vector<int>& v) {
     
-        vector<int> w;
+        long long int max1 = LONG_MIN;
+        long long int max2 = LONG_MIN;
+        long long int max3 = LONG_MIN;
         
-        for(auto &x : v) {
-            if(find(w.begin(), w.end(), x) == w.end())          w.push_back(x);
-        }                    
+        bool flag = false;
+        int n = v.size();
         
-        int n = w.size();
-        sort(w.begin(), w.end());
-        
-        if(n >= 3)                                              return w[n-3];
-        
-        return w[n-1];
+        for(int i=0; i<n; i++) {
+            
+            if(v[i] == INT_MIN)                         flag = true;
+            
+            if(v[i] > max1) {
+                
+                max3 = max2;
+                max2 = max1;
+                max1 = v[i];
+            }
+            
+            else if(v[i] > max2 && v[i] != max1) {
+                max3 = max2;
+                max2 = v[i];
+            }
+            
+            else if(v[i] > max3 && v[i] != max1 && v[i] != max2) {
+                max3 = v[i];
+            }   
+        }
+       
+        if(max3 == LONG_MIN)                             return max1;
+        else                                             return max3;
     }
     
 };
