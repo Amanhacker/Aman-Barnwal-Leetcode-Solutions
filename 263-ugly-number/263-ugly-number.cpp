56fc475ed46c68d33b1e0c967452f0f87312846a
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-16 04:02:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (20.79%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -3,12 +3,15 @@ public:
     
     bool isUgly(int n) {
     
+        vector<int> primes = {2, 3, 5};
+        
         // Find all the prime factors of number
         if(n <= 0)                              return false;
-
-        while(n % 2 == 0)                       n = n / 2;
-        while(n % 3 == 0)                       n = n / 3;
-        while(n % 5 == 0)                       n = n / 5;
+        if(n == 1)                              return true;
+        
+        for(auto &x : primes) {
+            while(n % x == 0)                   n = n / x;
+        }
         
         return (n == 1);
     }
