d6b32245f7701d16c010996df1263ddd9b8faaf2
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-17 18:47:51 +0530

    Time: 9 ms (17.60%), Space: 6.7 MB (40.89%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -8,13 +8,16 @@ public:
         
         for(int i=0; i<n; i++)                              pos[s[i] - 'a'] = i;
         
-        int i=0, ind = INT_MIN, lastInd = 0;
+        int maxInd = INT_MIN, lastInd = 0;
         
-        for( ; i < n; i++) {
-
-            ind = max(ind, pos[s[i] - 'a']);
+        for(int i=0; i<n; i++){
             
-            if(ind == i)                                    res.push_back(i - exchange(lastInd, i + 1) + 1);    
+            maxInd = max(maxInd, pos[s[i] - 'a']);
+            
+            if(maxInd == i) {
+                res.push_back(maxInd - lastInd + 1);
+                lastInd = i + 1;
+            }
         }
         
         return res;
