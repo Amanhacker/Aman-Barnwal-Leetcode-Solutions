1040d0bee67da8a8a403e71a74aa020cd6d4aaf3
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-17 18:54:32 +0530

    Time: 4 ms (76.85%), Space: 6.7 MB (40.89%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -6,9 +6,13 @@ public:
         vector<int> res, pos(26, 0);
         int n = s.length();
         
+        // We record the last position for each letter
         for(int i=0; i<n; i++)                              pos[s[i] - 'a'] = i;
         
         int maxInd = INT_MIN, lastInd = 0;
+        
+        // we keep the maximum position of the letters we have seen so far. 
+        // If the pointer exceeds the maximum position, we found the part.
         
         for(int i=0; i<n; i++){
             
