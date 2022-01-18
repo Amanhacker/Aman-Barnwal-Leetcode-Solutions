8e5a7c5c18f30fad42452106f847f87dd38f1373
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-18 18:54:49 +0530

    Time: 7 ms (32.97%), Space: 10 MB (26.96%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+    
+        // Here, If you're travelling today then 3 choices : 1-day, 7-day, or 30-day pass.
+
+        int n = days.size();
+        vector<int> dp (366, 0);
+        
+        // dp(i) = cost to fulfill your travel plan from day 1 to day i. 
+        
+        unordered_set<int> travel(days.begin(), days.end());
+        
+        for(int i=1; i<=365; i++) {
+            
+            if(travel.find(i) == travel.end())                      dp[i] = dp[i-1];
+            
+            else {
+                dp[i] = min({dp[max(0, i - 1)] + costs[0], dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2]}); 
+            }   
+        }
+        
+        return dp[365];
+    }
+    
+};
