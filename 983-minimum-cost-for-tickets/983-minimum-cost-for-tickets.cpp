d0b0d475ec88024e40caa21799caa207ef6ccf9d
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-18 18:59:53 +0530

    Time: 14 ms (9.56%), Space: 9.5 MB (71.29%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -12,7 +12,12 @@ public:
         
         for(int i=1; i<=365; i++) {
             
-            if(find(days.begin(), days.end(), i) == days.end())                      dp[i] = dp[i-1];
+            // For non-travel days, the cost stays the same as for the previous day. 
+            
+            if(find(days.begin(), days.end(), i) == days.end())              dp[i] = dp[i-1];
+            
+            // For travel days, it's a minimum of yesterday's cost plus single-day ticket, 
+            // or cost for 8 days ago plus 7-day pass, or cost 31 days ago plus 30-day pass.
             
             else {
                 dp[i] = min({dp[max(0, i - 1)] + costs[0], dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2]}); 
