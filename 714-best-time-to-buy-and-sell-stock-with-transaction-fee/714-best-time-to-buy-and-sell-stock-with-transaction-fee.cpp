61bdd69cfea6c64a00c7b4982014691e00b2dfa6
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-19 17:16:52 +0530

    Time: 137 ms (41.75%), Space: 55.1 MB (56.14%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
   
-    int maxProfit(vector<int>& prices, int fee) {
+    int maxProfit(vector<int>& a, int fee) {
     
-        int n = prices.size();
+        int n = a.size();
         
-        int cash = 0;
-        int hold = -prices[0];                // Buy here first stock initially
+        int oldBuyProfit = -a[0];
+        int oldSellProfit = 0;                // Buy here first stock initially
         
         // Initially we have negative hold cash because we have to buy first before making any profit
         
         for(int i=1; i<n; i++) {
             
-            cash = max(cash, hold + prices[i] - fee);               // Sell the stock
-            hold = max(hold, cash - prices[i]);                     // Holding the stock
+            int newBuyProfit = 0, newSellProfit = 0;
+            
+            if(oldSellProfit - a[i] > oldBuyProfit)             newBuyProfit = oldSellProfit - a[i];
+            else                                                newBuyProfit = oldBuyProfit;
+            
+            if(oldBuyProfit + a[i] - fee > oldSellProfit)       newSellProfit = oldBuyProfit + a[i] - fee;
+            else                                                newSellProfit = oldSellProfit;
+            
+            oldBuyProfit = newBuyProfit;
+            oldSellProfit = newSellProfit;
         }
         
-        return cash;
+        return oldSellProfit;
     }
     
 };
