aa03e434aff6fd9cf9dc932646ef23bde3265165
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-19 15:30:11 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (85.76%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -6,56 +6,14 @@ public:
         int count = 0;
         int n = a.size();
         
-        if(n <= 1)                                      return 0;  
-        
-        int diff = a[1] - a[0];
-        int start = 0;
-        
-        for(int i=1; i<n; i++) {
+        for(int i=2, j=1; i<n; i++) {
             
-            int start = i - 1;
-            bool flag = false;
+            if(a[i] - a[i-1] != a[j] - a[j-1])                      j = i;
             
-            while(i < n && a[i] - a[i-1] == diff) {
-                i++;
-                flag = true;
-            }
-            
-            if(i < n && a[i] - a[i-1] != diff) {
-                diff = a[i] - a[i-1];
-            }
-            
-            int end = i - 1;
-            
-            if(flag == true) {
-               
-                // Equal subarray is from [start, end]
-          
-                int len = end - start + 1;
-                
-                if(len == 3)                    count += 1;
-                
-                if(len > 3) {
-                  
-                    // No of subarrays of len 3 = len - 3 + 1
-                    // No of subarrays of len 4 = len - 4 + 1, and so on
-                    
-                    int temp = 3;
-                    
-                    while(len - temp + 1 > 0) {
-                        count += (len - temp + 1);
-                        temp++;
-                    }
-                }   
-                
-                if(i == n)                      break;
-                i = i - 1;
-            }
+            count += (i - j);
         }
         
         return count;
     }
     
 };
-
-// 1 2 3 5 6
