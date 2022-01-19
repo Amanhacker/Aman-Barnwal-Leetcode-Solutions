a1fa18f030561fe5afbbf14a66fac51b95c92c4d
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-19 15:33:04 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (52.48%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -6,14 +6,52 @@ public:
         int count = 0;
         int n = a.size();
         
-        for(int i=2, j=1; i<n; i++) {
+        if(n <= 1)                                      return 0;  
+        
+        int diff = a[1] - a[0];
+        int start = 0;
+        
+        for(int i=1; i<n; i++) {
             
-            if(a[i] - a[i-1] != a[j] - a[j-1])                      j = i;
+            int start = i - 1;
+            bool flag = false;
             
-            count += (i - j);
+            while(i < n && a[i] - a[i-1] == diff) {
+                i++;
+                flag = true;
+            }
+            
+            if(i < n && a[i] - a[i-1] != diff) {
+                diff = a[i] - a[i-1];
+            }
+            
+            int end = i - 1;
+            
+            if(flag == true) {
+               
+                // Equal subarray is from [start, end]
+          
+                int len = end - start + 1;
+                
+                if(len == 3)                    count += 1;
+                
+                if(len > 3) {
+                  
+                    // No of subarrays of len 3 = len - 3 + 1
+                    // No of subarrays of len 4 = len - 4 + 1, and so on
+             
+                    // Possible subarrays from Longest subarray length K is (k-2)*(K-1)/ 2
+                    count += (len - 2) * (len - 1) / 2;
+                }   
+                
+                if(i == n)                      break;
+                i = i - 1;
+            }
         }
         
         return count;
     }
     
 };
+
+// 1 2 3 5 6
