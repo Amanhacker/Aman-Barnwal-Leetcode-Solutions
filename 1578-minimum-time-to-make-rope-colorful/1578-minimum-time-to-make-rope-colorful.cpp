17218e3894a2bc5eed65f8b973e0f3127d674e93
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-19 14:46:22 +0530

    Time: 200 ms (36.41%), Space: 95.5 MB (48.03%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    int minCost(string a, vector<int>& neededTime) {
+    
+        int time = 0;
+        int n = a.size();
+        
+        for(int i=0; i<n; ) {
+            
+            // Find whether the same colors are consecutive
+            
+            int sum = neededTime[i];
+            int maxi = neededTime[i];
+        
+            bool flag = true;
+            int j = i;
+            
+            while(j + 1 < n && a[j] == a[j + 1]) {
+                
+                sum += neededTime[j + 1];
+                
+                if(maxi < neededTime[j + 1]) {
+                    maxi = max(maxi, neededTime[j + 1]);
+                }
+                
+                flag = false;
+                j++;
+            }
+            
+            if(flag == false) {
+                
+                time += sum - maxi;
+                cout << sum << " " << maxi << " " << time << " ";
+
+                i = j + 1;
+            }
+            
+            else                    i++;
+        }
+        
+        return time;
+    }
+    
+};
