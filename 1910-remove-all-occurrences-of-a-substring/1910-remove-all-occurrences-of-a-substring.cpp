a21155a54e642d0c78e526daf20d74383ba46e3a
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-20 18:57:34 +0530

    Time: 4 ms (79.23%), Space: 6.8 MB (25.92%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -12,12 +12,7 @@ public:
             auto ind = s.find(part);
             
             if(ind == string :: npos)                              return s;
-            
-            else {
-                
-                // ind is the starting index of leftmost occurence of string part in string s
-                s.erase(ind, len);        
-            }
+            else                                                   s.erase(ind, len);        
         }
         
         return s;
