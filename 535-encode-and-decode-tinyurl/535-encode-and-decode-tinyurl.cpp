f8ec0698f2c56db2fe57303a4f7ad46d4ab550d3
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-28 13:25:28 +0530

    Time: 8 ms (26.64%), Space: 7 MB (43.49%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
 
+    unordered_map<string, string> u;
+    int n = 0;
+    
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
         
-        return longUrl;
+        string encoded = "http://tinyurl.com/";
+        n++;
+        
+        encoded += to_string(n);
+        u[encoded] = longUrl;
+        
+        return encoded;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
     
-        return shortUrl;
-        
+        return u[shortUrl];
     }
     
 };
