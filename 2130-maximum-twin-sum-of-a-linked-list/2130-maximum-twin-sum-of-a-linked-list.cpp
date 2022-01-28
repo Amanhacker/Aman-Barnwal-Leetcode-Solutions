de8ab3f29b7fb69ecfddc0fd8d462eabbfdf6656
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-01-28 13:18:37 +0530

    Time: 297 ms (75.10%), Space: 116.7 MB (88.17%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,20 +11,51 @@
 class Solution {
 public:
     
+    ListNode* reverseList(ListNode* head) {
+        
+        ListNode* cur = head;
+        
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        // Reverse the Linked List starting from (n / 2)th node in original Linked List
+        while(cur != NULL) {
+            
+            next = cur->next;
+            cur->next = prev;
+            
+            prev = cur;
+            cur = next;
+        }
+        
+        return prev;
+    }
+    
     int pairSum(ListNode* head) {
     
         int res = INT_MIN;
         
-        vector<int> v;
+        ListNode* slow = head;
+        ListNode* fast = head;
         
-        while(head != NULL) {
-            v.push_back(head->val);
-            head = head->next;
+        while(fast != NULL && fast->next != NULL) {
+            
+            fast = fast->next->next;
+            slow = slow->next;
         }
         
-        int n = v.size();
+        // Here, fast becomes NULL and slow is (n/2)th node, considering 0-indexing
+        // Now, reverse the Linked List after half nodes from index [n/2, n)
         
-        for(int i=0, j=n-1; i<j; i++, j--)                                      res = max(res, v[i] + v[j]);
+        ListNode* otherHead = reverseList(slow);
+        
+        while(otherHead != NULL) {
+            
+            res = max(res, otherHead->val + head->val);
+            
+            otherHead = otherHead->next;
+            head = head->next;
+        }
         
         return res;
     }
