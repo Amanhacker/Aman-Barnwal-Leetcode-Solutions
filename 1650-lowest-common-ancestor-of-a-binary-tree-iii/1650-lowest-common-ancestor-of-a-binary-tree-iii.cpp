879d2a11cbd532166d3614f55abac634780fbe70
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-01 15:07:28 +0530

    Time: 12 ms (89.43%), Space: 10 MB (95.88%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+    
+        if(p == NULL || q == NULL)                 return NULL;
+        
+        Node* p1 = p;
+        Node* q1 = q;
+        
+        while(p1 != q1) {
+            
+            if(p1 == NULL)                         p1 = q;
+            else                                   p1 = p1->parent;
+            
+            if(q1 == NULL)                         q1 = p;
+            else                                   q1 = q1->parent;
+        }        
+
+        return p1;
+    }
+    
+};
