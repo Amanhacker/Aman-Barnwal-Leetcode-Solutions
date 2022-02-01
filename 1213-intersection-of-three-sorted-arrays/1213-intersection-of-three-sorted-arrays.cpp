86cd3163bc3e601ebce3c2a75b9c22111476b684
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-02 01:32:56 +0530

    Time: 21 ms (23.82%), Space: 11.9 MB (54.09%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
@@ -5,14 +5,27 @@ public:
     
         vector<int> res;
         
-        for(int i=0; i<a1.size(); i++) {
+        int i = a1.size() - 1;
+        int j = a2.size() - 1;
+        int k = a3.size() - 1;
+        
+        while(i >= 0 && j >= 0 && k >= 0) {
             
-            auto it2 = find(a2.begin(), a2.end(), a1[i]);
-            auto it3 = find(a3.begin(), a3.end(), a1[i]);
-            
-            if(it2 != a2.end() && it3 != a3.end())                          res.push_back(a1[i]);
+           if(a1[i] > a2[j] || a1[i] > a3[k])                              i--;
+           else if(a2[j] > a3[k] || a2[j] > a1[i])                         j--;
+           else if(a3[k] > a1[i] || a3[k] > a2[j])                         k--;
+          
+           else {
+               
+               res.push_back(a1[i]);
+               
+               i--;
+               j--;
+               k--;
+           }
         }
         
+        reverse(res.begin(), res.end());
         return res;
     }
     
