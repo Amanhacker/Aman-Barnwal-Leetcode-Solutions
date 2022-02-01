1cd45ee68944cf384469723ee0e10d1209d66ae2
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-01 13:08:38 +0530

    Time: 3 ms (85.98%), Space: 14.1 MB (5.56%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
@@ -12,33 +12,17 @@
 class Solution {
 public:
     
-    unordered_map<int, vector<int>> mp;
-    unordered_map<TreeNode*, int> u;
-    
-    int height(TreeNode* root) {
-        
-        if(root == NULL)                                               return 0;
-        
-        int l = height(root->left);
-        int r = height(root->right);
-        
-        return 1 + max(l, r);
-    }
-    
-    // Level Order Traversal
-    void levelOrder(TreeNode* root, int depth, int lev) {
+    map<int, map<int, vector<int>>> mp;
+
+    // DFS Traversal
+    void dfs(TreeNode* root, int row, int col) {
         
         if(root == NULL)                                               return;
         
-        u[root] = depth;
-            
-        if(lev == 1)                                                    mp[depth].push_back(root->val);
-            
-        else if(lev > 1) {
+        mp[col][row].push_back(root->val);
         
-            if(root->left != NULL)                                      levelOrder(root->left, u[root] - 1, lev - 1);
-            if(root->right != NULL)                                     levelOrder(root->right, u[root] + 1, lev - 1);
-        }
+        dfs(root->left, row + 1, col - 1);
+        dfs(root->right, row + 1, col + 1);
         
         return;
     }
@@ -50,20 +34,20 @@ public:
         if(root == NULL)                                                return res;
         
         // Find Vertical Order Traversal of the given BT
-        unordered_map<TreeNode*, int> u;
-        vector<pair<int, vector<int>>> v;
+        dfs(root, 0, 0);
         
-        int h = height(root);
-        int dep = 0;
-        
-        for(int i=1; i<=h; i++) {
-            levelOrder(root, u[root], i);
-        }   
-                
-        for(auto &x : mp)                                               v.push_back({x.first, x.second});
-        sort(v.begin(), v.end());
-        
-        for(auto &x : v)                                                res.push_back(x.second);    
+        for(auto &x : mp) {
+            
+            map<int, vector<int>> u = x.second;
+            vector<int> temp;
+            
+            for(auto &x1 : u) {
+                for(auto &x2 : x1.second)                              temp.push_back(x2);
+            }
+            
+            res.push_back(temp);
+            temp.clear();
+        }
         
         return res;
     }
