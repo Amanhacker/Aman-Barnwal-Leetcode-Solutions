c568410dd8e0a07497f6ec2f87a8fcacb113d33e
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-02 00:58:53 +0530

    Time: 12 ms (61.14%), Space: 10.6 MB (16.57%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.cpp
+++ 1064-fixed-point/1064-fixed-point.cpp
@@ -3,11 +3,27 @@ public:
    
     int fixedPoint(vector<int>& a) {
     
-        for(int i=0; i<a.size(); i++) {
-            if(a[i] == i)                               return i;
+        int res = -1;
+        
+        // Find the smallest index i such that a[i] = i
+        int n = a.size();
+        
+        int start = 0, end = n - 1;
+        
+        while(start <= end) {
+            
+            int mid = start + (end - start) / 2;
+            
+            if(a[mid] == mid) {
+                res = mid;
+                end = mid - 1;
+            }
+            
+            else if(a[mid] < mid)                               start = mid + 1;
+            else                                                end = mid - 1;
         }
         
-        return -1;
+        return res;
     }
     
 };
