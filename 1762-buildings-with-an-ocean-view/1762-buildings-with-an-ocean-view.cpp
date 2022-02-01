95b393913d01f2058fe818c15a72bceb6af1cb44
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-01 12:04:08 +0530

    Time: 304 ms (5.95%), Space: 68.5 MB (98.44%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -6,22 +6,11 @@ public:
         vector<int> res, maxR;
         int n = a.size();
         
-        int maxi = a[n-1];
-        maxR.resize(n);
-        
-        // Here, maxR[i] is the maximum element from [i + 1, n - 1]
-        maxR[n-1] = 0;
-        
-        for(int i=n-2; i>=0; i--) {
-            maxi = max(maxi, a[i + 1]);
-            maxR[i] = maxi;
+        for(int i=n-1; i>=0; i--) {
+            if(res.empty() == true || a[i] > a[res.back()])                res.push_back(i);
         }
         
-        for(int i=0; i<n-1; i++) {
-            if(a[i] > maxR[i])                             res.push_back(i);
-        }
-        
-        res.push_back(n-1);
+        reverse(res.begin(), res.end());
         return res;
     }
     
