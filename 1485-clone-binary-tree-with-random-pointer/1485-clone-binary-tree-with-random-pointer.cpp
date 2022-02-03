c6373dd1988c3682cdf4e64b6e5177825870ea66
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-03 16:47:22 +0530

    Time: 367 ms (41.53%), Space: 76.1 MB (54.48%) - LeetHub

---
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     Node *left;
+ *     Node *right;
+ *     Node *random;
+ *     Node() : val(0), left(nullptr), right(nullptr), random(nullptr) {}
+ *     Node(int x) : val(x), left(nullptr), right(nullptr), random(nullptr) {}
+ *     Node(int x, Node *left, Node *right, Node *random) : val(x), left(left), right(right), random(random) {}
+ * };
+ */
+
+class Solution {
+public:
+    
+    // Using map
+
+    NodeCopy* helpCopyRandom(Node* root, unordered_map<Node*, NodeCopy*> &u) {
+        
+        if(root == NULL)                               return NULL;
+        
+        if(u.count(root) == 1)                         return u[root];
+        
+        NodeCopy* newRoot = new NodeCopy(root->val);
+        u[root] = newRoot;
+        
+        newRoot->left   = helpCopyRandom(root->left, u);
+        newRoot->right  = helpCopyRandom(root->right, u);
+        newRoot->random = helpCopyRandom(root->random, u);
+        
+        return newRoot;
+    }
+    
+    NodeCopy* copyRandomBinaryTree(Node* root) {
+    
+        NodeCopy* newRoot = NULL;
+        
+        if(root == NULL)                                return newRoot;
+        
+        unordered_map<Node*, NodeCopy*> u;
+        
+        newRoot = helpCopyRandom(root, u);
+        return newRoot;
+    }
+    
+};
