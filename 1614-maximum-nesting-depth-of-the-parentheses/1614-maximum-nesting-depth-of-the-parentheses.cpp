8c2816811fb32d1147f90c01766eb382d2c3eea2
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-03 15:14:38 +0530

    Time: 7 ms (5.45%), Space: 6.1 MB (86.91%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -6,6 +6,9 @@ public:
         int res = 0, cur = 0;
         int n = s.length();
         
+        // Ignore digits and signs, only count the current open parentheses cur.
+        // The depth equals to the maximum open parentheses.
+        
         for(int i=0; i<n; i++) {
             
             if(s[i] == '(') {
