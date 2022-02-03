f54ca4017d45e0fe2dcf462837e362168da6bd19
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-03 13:32:10 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (30.44%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    vector<string> helperWordBreak(string &s, int ind, int n, unordered_set<string> &word, unordered_map<int, vector<string>> &u) {
+        
+        vector<string> res;
+        
+        // Base Condition
+        if(ind == n) {
+            res.push_back(" ");
+            return res;
+        }
+        
+        if(u.count(ind))                                   return u[ind];
+        
+        string temp = "";
+        vector<string> sub;
+        
+        for(int i=ind; i<n; i++) {
+            
+            temp += s[i];
+            
+            // If set has index, where word didn't break
+            if(word.count(temp)) {
+                
+                 sub = helperWordBreak(s, i + 1, n, word, u);
+            
+                 for(auto x : sub) {
+
+                      if(x == " ")                               res.push_back(temp);
+                      else                                       res.push_back(temp + " " + x);
+                 }
+            }
+        }
+        
+        u[ind] = res;
+        return res;
+    }
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+    
+        int n = s.length();
+        
+        unordered_set<string> word;
+        unordered_map<int, vector<string>> w;
+        
+        for(auto &x : wordDict)                             word.insert(x);
+        
+        return helperWordBreak(s, 0, n, word, w);
+    }
+    
+};
