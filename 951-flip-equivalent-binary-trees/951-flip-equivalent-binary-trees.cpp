844608ea8e29ae681b111ab18b4443cf22d6745f
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-04 22:53:07 +0530

    Time: 11 ms (10.42%), Space: 12.3 MB (24.42%) - LeetHub

---
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+    
+        if(root1 == root2)                                                  return true;
+        
+        if(root1 == NULL && root2 == NULL)                                  return true;
+        if(root1 == NULL || root2 == NULL || root1->val != root2->val)      return false;
+        
+        bool f1 = flipEquiv(root1->left, root2->left)  && flipEquiv(root1->right, root2->right);
+        bool f2 = flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left); 
+        
+        return (f1 || f2);
+    }
+    
+};
