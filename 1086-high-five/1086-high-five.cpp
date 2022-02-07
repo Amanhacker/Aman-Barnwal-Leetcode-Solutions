65b3979832838956ac03b013e9d275d0788ecc7c
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-07 21:55:08 +0530

    Time: 7 ms (98.74%), Space: 11.3 MB (18.39%) - LeetHub

---
+++ 1086-high-five/1086-high-five.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    
+    vector<vector<int>> highFive(vector<vector<int>>& a) {
+    
+        vector<vector<int>> res;        
+        
+        int n = a.size();
+        
+        unordered_map<int, vector<int>> u;
+        
+        for(auto &x : a) {
+            
+            vector<int> temp = x;
+            u[temp[0]].push_back(temp[1]);
+        }
+        
+        for(auto &x : u) {
+            
+            vector<int> t = x.second;
+            sort(t.begin(), t.end(), greater<int>());
+            
+            int sum = 0;
+            for(int i=0; i<5; i++)                          sum += t[i];
+
+            vector<int> path;
+            
+            path.push_back(x.first);
+            path.push_back(sum / 5);
+            
+            res.push_back(path);
+            path.clear();
+        }
+        
+        sort(res.begin(), res.end());
+        return res;
+    }
+    
+};
