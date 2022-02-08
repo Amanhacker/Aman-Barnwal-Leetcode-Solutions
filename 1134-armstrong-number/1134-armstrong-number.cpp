c767ef08efb196e54f51b5cd034ea1cc93aa1def
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-08 22:27:55 +0530

    Time: 2 ms (22.66%), Space: 5.9 MB (96.09%) - LeetHub

--- 1134-armstrong-number/1134-armstrong-number.cpp
+++ 1134-armstrong-number/1134-armstrong-number.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-   
+    
+    // To find the power of a^m
+    int findPower(int a, int m) {
+        
+        if(m == 0)                                      return 1;
+        
+        else if(m % 2 == 0)                             return findPower(a, m / 2) * findPower(a, m / 2);
+        else if(m % 2 == 1)                             return a * findPower(a, m - 1);
+        
+        return -1;
+    }
+    
     bool isArmstrong(int n) {
     
         int k = 0;
@@ -18,7 +29,7 @@ public:
         int sum = 0;
         
         for(auto &x : v) {
-            sum += pow(x, k);
+            sum += findPower(x, k);
         }
         
         return (sum == n);
