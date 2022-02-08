ec48e6a2ea2ca6302d2a9a1bf25aca7a76386b35
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-08 16:52:45 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.62%) - LeetHub

---
+++ 246-strobogrammatic-number/246-strobogrammatic-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    bool isStrobogrammatic(string s) {
+    
+        int n = s.length();
+        string res = s;
+        
+        for(int i=0; i<n; i++) {
+           
+            if(s[i] == '6')                                         s[i] = '9';
+            else if(s[i] == '9')                                    s[i] = '6';
+            else if(s[i] == '0' || s[i] == '8' || s[i] == '1')      continue;
+            else                                                    return false;
+        }
+        
+        reverse(res.begin(), res.end());
+        return (res == s);
+    }
+    
+};
