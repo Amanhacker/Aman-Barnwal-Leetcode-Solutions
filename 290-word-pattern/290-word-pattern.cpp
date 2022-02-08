a439606236f1837a8ce304f0eb1d208fda82687b
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-08 19:54:06 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.61%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    
+    bool wordPattern(string pat, string s) {
+    
+        vector<string> v;
+        
+        // Breaking input into word using string stream    
+        
+        stringstream ss(s);                                          // Used for breaking words
+        string word;                                                 // To store individual words
+        
+        while(ss >> word) {
+            v.push_back(word);
+        }
+    
+        map<int, int> u;
+        vector<int> r1, r2;
+        
+        int c = 0;
+        
+        for(int i=0; i<pat.length(); i++) {
+
+            if(u[pat[i]] == 0) {
+                
+                u[pat[i]]++;
+                
+                c++;
+                u[pat[i]] = c;
+            }   
+            
+            r1.push_back(u[pat[i]]);
+        }
+        
+        map<string, int> u2;
+        c = 0;
+        
+        for(int i=0; i<v.size(); i++) {
+           
+            if(u2[v[i]] == 0) {
+                
+                u2[v[i]]++;
+                
+                c++;
+                u2[v[i]] = c;
+            }   
+            
+            r2.push_back(u2[v[i]]);
+        }
+        
+        for(auto &x : r1)                          cout << x << " ";
+        cout << endl;
+            
+        for(auto &x : r2)                          cout << x << " ";
+        cout << endl;
+        
+        return (r1 == r2);
+    }
+    
+};
