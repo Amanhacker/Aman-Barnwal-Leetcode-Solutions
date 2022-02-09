98522d50ac03f884328ec55a81f26397bc5c6faa
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-10 02:54:14 +0530

    Time: 3 ms (73.96%), Space: 8.4 MB (66.86%) - LeetHub

---
+++ 1628-design-an-expression-tree-with-evaluate-function/1628-design-an-expression-tree-with-evaluate-function.cpp
@@ -0,0 +1,129 @@
+/**
+ * This is the interface for the expression tree Node.
+ * You should not remove it, and you can define some classes to implement it.
+ */
+
+class Node {
+public:
+    virtual ~Node () {};
+    virtual int evaluate() const = 0;
+protected:
+    // define your fields here
+};
+
+
+/**
+ * This is the TreeBuilder class.
+ * You can treat it as the driver code that takes the postinfix input 
+ * and returns the expression tree represnting it as a Node.
+ */
+
+class NumNode: public Node {
+    
+    public:
+        
+        NumNode(int val) : val(val) {};
+        
+        int evaluate() const {
+            return val;
+        };
+    
+    protected:
+        
+        int val;
+};
+
+class OpNode: public Node {
+   
+    public:
+        
+        OpNode(char op, Node *left, Node *right) : op(op), left(left), right(right) {};
+        int evaluate() const;
+    
+    protected:
+        
+        char op;
+        Node *left;
+        Node *right;
+};
+
+
+int OpNode::evaluate() const {
+   
+    switch(this->op) {
+       
+        case '+':
+            return left->evaluate() + right->evaluate(); 
+        
+        case '-':
+            return left->evaluate() - right->evaluate(); 
+        
+        case '*':
+            return left->evaluate() * right->evaluate(); 
+        
+        case '/':
+            return left->evaluate() / right->evaluate(); 
+        
+        default:
+            return 0;
+    }
+}
+
+// class TreeBuilder {
+// public:
+//     Node* buildTree(vector<string>& postfix) {
+//         stack<Node*> st;
+//         for (auto s: postfix) {
+//             if(isdigit(s[0])) {
+//                 st.push(new NumNode(stoi(s)));
+//             }else{
+//                 auto r = st.top();
+//                 st.pop();
+//                 auto l = st.top();
+//                 st.pop();
+//                 st.push(new OpNode(s[0], l, r));
+//             }
+//         }
+//         return st.top();
+//     }
+// };
+
+class TreeBuilder {
+public:
+    
+    // Build the expression tree
+    Node* buildTree(vector<string>& postfix) {
+        
+        stack<Node*> st;
+        
+        for (auto s: postfix) {
+        
+            if(isdigit(s[0])) {
+            
+                st.push(new NumNode(stoi(s)));
+            }
+            
+            else{
+            
+                auto right = st.top();
+                st.pop();
+                
+                auto left = st.top();
+                st.pop();
+                
+                st.push(new OpNode(s[0], left, right));
+            }
+        }
+        
+        return st.top();
+    }
+    
+};
+
+
+/**
+ * Your TreeBuilder object will be instantiated and called as such:
+ * TreeBuilder* obj = new TreeBuilder();
+ * Node* expTree = obj->buildTree(postfix);
+ * int ans = expTree->evaluate();
+ */
