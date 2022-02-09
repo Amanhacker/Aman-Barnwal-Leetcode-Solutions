68d96bf4f40c2a713006afc5d922b40f00e8dadf
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-09 07:26:20 +0530

    Time: 7 ms (83.80%), Space: 12.4 MB (20.25%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    vector<int> sortEvenOdd(vector<int>& a) {
+    
+        vector<int> res;
+        int n = a.size();
+        
+        vector<int> oddInd, evenInd;
+        
+        for(int i=0; i<n; i++) {
+            if(i % 2 == 1)                                      oddInd.push_back(a[i]);
+            else                                                evenInd.push_back(a[i]);
+        }
+
+        sort(evenInd.begin(), evenInd.end());
+        sort(oddInd.begin(), oddInd.end(), greater<int>());        
+        
+        int k = 0;
+        
+        for(int i=0, j=0; i < evenInd.size() || j < oddInd.size(); i++, j++, k += 2) {
+            if(i < evenInd.size())                               res.push_back(evenInd[i]);
+            if(j < oddInd.size())                                res.push_back(oddInd[j]);
+        }        
+        
+        return res;
+    }
+    
+};
