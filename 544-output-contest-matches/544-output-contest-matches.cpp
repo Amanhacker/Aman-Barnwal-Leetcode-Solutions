f3378e3a66322da729e65e70f8cda1c1ed446bde
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-12 08:10:47 +0530

    Time: 11 ms (40.32%), Space: 15.2 MB (6.45%) - LeetHub

---
+++ 544-output-contest-matches/544-output-contest-matches.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    string findContestMatch(int n) {
+    
+        string res = "";
+        
+        // Pair according to rank in each scanario
+        
+        stack<string> st;
+        map<int, string> mp;
+        
+        for(int i=1, j=n, k=1; i<j; i++, j--) {
+            
+            // Make the pair (i, j)
+            string temp = "(" + to_string(i) + "," + to_string(j) + ")";
+            
+            mp[k] = temp;
+            k++;
+        }
+        
+        
+        // First round of pair is already done
+            
+        while(true) {
+
+            vector<string> v;
+            for(auto &x : mp)                           v.push_back(x.second);
+
+            if(v.size() == 1)                           break;
+
+            map<int, string> u;
+            int k = 1;
+
+            for(int i=0, j=v.size()-1; i<j; i++, j--) {
+
+                string temp = "(" + v[i] + "," + v[j] + ")";
+
+                u[k] = temp;
+                k++;
+            }
+
+            mp = u;
+            v.clear();
+        }
+        
+        res += mp[1];
+        
+        return res;
+    }
+    
+};
