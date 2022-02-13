1a32a8c172682ede70f9ed50e1288da6bd2e5d5c
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-13 22:01:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (60.98%) - LeetHub

--- 1256-encode-number/1256-encode-number.cpp
+++ 1256-encode-number/1256-encode-number.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     
+    
     string encode(int num) {
     
         string res = "";
+
+        // Here, n is affecting the (2n + 1) and (2n + 2). 
+        // Hence, we can at each step divide the number by (n - 1) / 2, to reach back to the old state.
+
+        // If it is divisible by 2, we append 1. Else 0.
         
-        res = bitset<32>(num + 1).to_string().substr(32-(int)log2(num + 1));
+        while(num >= 2) {
+            
+            if(num % 2 == 1)                          res += "0";
+            else                                      res += "1";
+            
+            num = (num - 1) / 2;
+        }
+        
+        if(num == 2)                                  res += "1";
+        if(num == 1)                                  res += "0";
+        
+        reverse(res.begin(), res.end());
         return res;
     }
     
