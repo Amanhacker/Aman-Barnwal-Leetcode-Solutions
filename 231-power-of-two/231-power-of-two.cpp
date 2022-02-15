dd232787713457b917f6734d4da1b57c9501560f
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-16 01:55:34 +0530

    Time: 7 ms (6.55%), Space: 5.6 MB (99.45%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    bool isPowerOfTwo(int n) {
+    
+        if(n == 1)                                      return true;
+        if(n <= 0)                                      return false;                           
+        
+        if(n % 2 == 0) {
+            
+            while(n >= 2 && n % 2 == 0) {
+                n = n / 2;
+            }
+            
+            cout << n << " ";
+            
+            return (n <= 2);
+        }
+        
+        else                                            return false;
+    }
+    
+};
