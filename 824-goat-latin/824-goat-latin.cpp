f3f02fcec833a3a79d39e6e0b6c0b71e4682fc61
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-16 01:02:13 +0530

    Time: 4 ms (41.21%), Space: 6.6 MB (36.60%) - LeetHub

---
+++ 824-goat-latin/824-goat-latin.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+   
+    bool isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+    }
+    
+    string toGoatLatin(string s) {
+    
+        string res = "";
+        vector<string> v;
+        
+        stringstream ss(s);
+        string word;
+        
+        while(ss >> word) {
+            v.push_back(word);
+            cout << word << " ";
+        }
+        
+        int n = v.size();
+        int c = 1;
+        
+        for(int i=0; i<n; i++) {
+            
+            string temp = v[i];
+
+            if(isVowel(temp[0]) == true) {
+                // Add ma at end of word
+                temp += "ma";
+            }
+            
+            else {
+                
+                // Remove the first letter and append at end
+                char ch = temp[0];
+                
+                temp.erase(temp.begin());
+                
+                temp += ch;
+                temp += "ma";
+            }
+            
+            // Add (c no.of a's) at the end of temp
+            int k = c;
+            
+            while(k--)                              temp += "a";
+            
+            if(i < n-1)                             res += temp + " ";
+            else                                    res += temp;
+            
+            c++;
+        }
+        
+        return res;
+    }
+    
+};
