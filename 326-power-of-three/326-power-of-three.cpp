367ede18609db86b2925ffe190ed66e7586c7fd5
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-16 01:59:24 +0530

    Time: 12 ms (86.23%), Space: 5.9 MB (74.09%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   
+    bool isPowerOfThree(int n) {
+    
+        if(n == 1)                                  return true;
+        if(n <= 0)                                  return false;
+        
+        if(n % 3 == 0) {
+            
+            while(n >= 3 && n % 3 == 0) {
+                n = n / 3;
+            }
+            
+            cout << n << " ";
+            return (n == 1);
+        }
+        
+        else                                        return false;
+    }
+    
+};
