60c6c0ee18ebbbdce19e82eaf4809b3f633f3579
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-16 00:45:31 +0530

    Time: 371 ms (5.10%), Space: 83.5 MB (6.56%) - LeetHub

---
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    bool lemonadeChange(vector<int>& bills) {
+    
+        int n = bills.size();
+        // sort(bills.begin(), bills.end());
+
+        map<int, int> mp;
+        
+        for(auto &x : bills) {
+            
+            
+            // He needs to give (x - 5) change to the customer
+            int change = (x - 5);
+            
+            if(change > 0) {
+               
+                if(change % 10 == 0) {
+
+                    if(mp[10] >= change / 10) {
+                        mp[10] -= change / 10;
+                        change -= 10 * (change / 10);
+                    }                         
+
+                    if(mp[5] >= change / 5) {
+                        mp[5] -= change / 5;
+                        change -= 5 * (change / 5);
+                    }
+
+                    if(change > 0)                                 return false;
+                }
+
+                else {
+                        
+                    if(change >= 10 && mp[10] >= change / 10) {
+                        mp[10] -= change / 10;
+                        change -= 10 * (change / 10);
+                    }   
+
+                    if(change >= 5 && mp[5] >= change / 5) {
+                        mp[5] -= change / 5;
+                        change -= 5 * (change / 5);
+                    }   
+
+                    if(change > 0)                              return false;
+                }                                            
+            }
+            
+            mp[x]++;
+            
+            // cout << x << " ";
+            cout << mp[5] << " " << mp[10] << " " << mp[20] << endl;
+        }
+        
+        
+        return true;
+    }
+    
+};
