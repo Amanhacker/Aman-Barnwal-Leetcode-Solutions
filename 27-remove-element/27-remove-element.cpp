3846260905d077a3e114ec7a837fec7fcc301b90
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-15 09:30:12 +0530

    Time: 3 ms (60.75%), Space: 8.6 MB (99.74%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    int removeElement(vector<int>& nums, int val) {
+    
+        int res = 0;
+        int n = nums.size();
+        
+        if(n == 0)                                                        return 0;
+        
+        int j = n - 1;
+
+        while(j >= 0 && nums[j] == val)                                              j--;
+
+        if(j < 0)                                                         return 0;
+        
+        for(int i=0; i<=j; i++) {
+            
+            while(j >= 0 && nums[j] == val)                                          j--;
+            
+            if(i > j)                                                      break;
+            
+            if(nums[i] == val) {
+                swap(nums[i], nums[j]);
+                j--;
+            }
+        }
+        
+        cout << j << " ";
+        
+        while(j >= 0 && nums[j] == val)                                              j--;
+
+        
+        // [0, j]
+        res = j + 1;
+        
+        nums.resize(res);
+        return res;
+    }
+    
+};
