88588775aa071bffba21870a6e1aea1ad6977695
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-16 18:47:20 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (17.06%) - LeetHub

--- 836-rectangle-overlap/836-rectangle-overlap.cpp
+++ 836-rectangle-overlap/836-rectangle-overlap.cpp
@@ -1,68 +1,25 @@
 class Solution {
 public:
-  
-    // Find the intersection interval of interval range
-    
-    int findIntersection(vector<vector<int>> interval) {
-
-        int res = 0;
-        int n = interval.size();
-        
-        int l = interval[0][0];
-        int r = interval[0][1];
-        
-        for(int i=1; i<n; i++) {
-            
-            if(interval[i][0] > r || interval[i][1] < l)                return -1;
-            
-            else {
-                
-                // Update l and r
-                
-                l = max(l, interval[i][0]);
-                r = min(r, interval[i][1]);
-            }
-        }
-        
-        res = abs(r - l);
-        return res;
-    }
     
     bool isRectangleOverlap(vector<int>& r1, vector<int>& r2) {
     
-        int x1, x2, y1, y2;
+        // Given 2 segment (left1, right1), (left2, right2), how can we check whether they overlap?
+        // If these two intervals overlap, it should exist an number x,
+
+        // left1 < x < right1 && left2 < x < right2
+        // left1 < x < right2 && left2 < x < right1
+
+        // left1 < right2 && left2 < right1 is the sufficient condition
+      
+        // For X-Coordinate
+        int left1 = r1[0], right1 = r1[2];
+        int left2 = r2[0], right2 = r2[2];
         
-        vector<vector<int>> xInterval, yInterval;
-        vector<int> path;
-        
-        path.push_back(r1[0]);
-        path.push_back(r1[2]);
-        
-        xInterval.push_back(path);
-        path.clear();
-        
-        path.push_back(r1[1]);
-        path.push_back(r1[3]);
-        
-        yInterval.push_back(path);
-        path.clear();
-        
-        path.push_back(r2[0]);
-        path.push_back(r2[2]);
-        
-        xInterval.push_back(path);
-        path.clear();
-        
-        path.push_back(r2[1]);
-        path.push_back(r2[3]);
-        
-        yInterval.push_back(path);
-        path.clear();
-        
-        int dx = findIntersection(xInterval);
-        int dy = findIntersection(yInterval);
-        
-        return (dx > 0 && dy > 0);
+        // For Y-Coordinate
+        int left3 = r1[1], right3 = r1[3];
+        int left4 = r2[1], right4 = r2[3];
+            
+        return ( (left1 < right2 && left2 < right1) && (left3 < right4 && left4 < right3) );
     }
     
 };
