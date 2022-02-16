5d838759820df92a907e2ceb6f38be23b4b1663b
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-16 17:49:32 +0530

    Time: 3 ms (74.84%), Space: 7.6 MB (93.35%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -6,26 +6,17 @@ public:
         int time = 0;
         int n = v.size();
         
-        map<int, int> mp;
-        
         while(v[k] > 0) {
 
             // Decrease each element of array by 1
             
             for(int i=0; i<n && v[k] > 0; i++) {
                 
-                if(v[i] == 0)                      mp[i] = 1;
-                
-                if(mp[i] == 0 && v[i] > 0) {
+                if(v[i] > 0) {
                     v[i]--;
                     time++;
                 }
             }
-            
-            cout << time << endl;
-            
-            for(auto &x : v)                        cout << x << " ";
-            cout << endl;
         }
         
         return time;
