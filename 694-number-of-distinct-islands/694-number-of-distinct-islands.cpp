d357d34b39dc7778ea6e98ed58af9c4d3c16870b
AMAN BARNWAL <aman25barnwal@gmail.com>
2022-02-18 17:01:44 +0530

    Time: 41 ms (75.65%), Space: 26.9 MB (79.92%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.cpp
@@ -3,88 +3,66 @@ public:
     
     // Possible movements are up, right, down, left
     // (-1, 0), (0, 1), (1, 0), (0, -1)
-//     int dx[4] = {-1, 0, 1,  0};
-//     int dy[4] = { 0, 1, 0, -1};
+    
+    int dx[4] = {-1, 0, 1,  0};
+    int dy[4] = { 0, 1, 0, -1};
 
-//     string s[4] = {"U", "R", "D", "L"};
+    string s[4] = {"U", "R", "D", "L"};
     
-//     bool isValid(int x, int y, int n, int m, vector<vector<int>> &vis, vector<vector<int>> &grid) {
+    bool isValid(int x, int y, int n, int m, vector<vector<int>> &grid) {
         
-//         if(x >= 0 && x < n && y >= 0 && y < m && vis[x][y] == 0 && grid[x][y] == 1)             return true;
-//         else                                                                                    return false;
-//     }
-    
-//     void bfs(int x, int y, int n, int m, vector<vector<int>> &vis, vector<vector<int>> &grid, set<string> &w) {
-        
-//         queue<pair<int, int>> q;
-//         q.push({x, y});
-        
-//         vis[x][y] = 1;
-        
-//         string str = "";
-        
-//         while(q.empty() == false) {
-            
-//             pair<int, int> temp = q.front();
-//             q.pop();
-
-//             for(int i=0; i<4; i++) {
-                
-//                 int x1 = temp.first + dx[i];
-//                 int y1 = temp.second + dy[i];
-                
-//                 if(isValid(x1, y1, n, m, vis, grid) == true) {
-                    
-//                     str += s[i];
-                    
-//                     q.push({x1, y1});
-//                     vis[x1][y1] = 1;
-//                 }
-//             }
-//         }
-
-//         cout << str << " ";
-//         w.insert(str);
-//         return;
-//     }
-    
-    int n, m;
-    set<string> ss;
-    
-    void dfs(vector<vector<int>> &a,int i,int j,string& s,string temp){
-        if(i<0||i>=n||j<0||j>=m) 
-            return;
-        if(a[i][j]==0) 
-            return;
-        a[i][j]=0;          // visted so changing 1 to 0 to avoid reconsideration of this 1 again
-        s+=temp;
-        dfs(a,i-1,j,s,"U");     // U upar
-        dfs(a,i+1,j,s,"N");     // N neeche
-        dfs(a,i,j-1,s,"B");     // B bayein/left
-        dfs(a,i,j+1,s,"D");     // D dayein/right
-        s+="K";                 // khatm pattern
+        if(x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 1)             return true;
+        else                                                                  return false;
     }
-
     
-    int numDistinctIslands(vector<vector<int>>& a) {
+    void dfs(int x, int y, int n, int m, vector<vector<int>> &grid, string &str) {
+       
+        // Base Condition
+        if(x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0)             return;
+        
+        grid[x][y] = 0;
+        
+        for(int i=0; i<4; i++) {
+
+            int x1 = x + dx[i];
+            int y1 = y + dy[i];
+
+            // if(isValid(x1, y1, n, m, grid) == true) {
+
+                str += s[i];
+                dfs(x1, y1, n, m, grid, str);
+            // }
+        }
+
+        return;
+    }
+    
+    int numDistinctIslands(vector<vector<int>>& grid) {
     
         int res = 0;
         
-        n = a.size();
-        m = a[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(a[i][j]!=0){
-                    string s="";
-                    dfs(a,i,j,s,"S");   // S shuru pattern 
-                    ss.insert(s);
+        set<string> w;
+        
+        // BFS on 2D Grid
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                
+                if(grid[i][j] == 1) {
+                    
+                    // Run DFS from (i, j)
+                    string str = "";
+                    dfs(i, j, n, m, grid, str);
+
+                    w.insert(str);
                 }
             }
         }
-        cout<<ss.size();
         
-        res = ss.size();
+        res = w.size();
         return res;
     }
     
