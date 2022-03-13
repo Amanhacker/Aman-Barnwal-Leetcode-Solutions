27a82e6e32cbfa4c9ecc3fc992c0890891526df9
AMAN <aman25barnwal@gmail.com>
2022-03-13 14:56:23 +0530

    Time: 357 ms (54.03%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
 
-SELECT Email 
-FROM (
-    SELECT Person.Email, COUNT(Email) as num
-    FROM Person
-    GROUP BY Email
-) AS statistic
-WHERE num > 1;
+# SELECT Email 
+# FROM (
+#     SELECT Person.Email, COUNT(Email) as num
+#     FROM Person
+#     GROUP BY Email
+# ) AS statistic
+# WHERE num > 1;
+
+
+SELECT Person.Email
+FROM Person
+GROUP BY Email
+HAVING COUNT(Email) > 1;
