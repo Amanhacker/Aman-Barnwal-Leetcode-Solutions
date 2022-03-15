b5995da0a45d492245f3ab2a5a85d697f752a1e1
AMAN <aman25barnwal@gmail.com>
2022-03-16 01:52:57 +0530

    Time: 8 ms (78.75%), Space: 10.6 MB (59.52%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -9,19 +9,11 @@ public:
         int start = 0, end = 0;
         int sum = 0;
         
-//         0 1 2 3 4  5
-        
-//         2 3 1 2 4  3
-//         2 5 6 8 12 15
-            
         while(start < n && end < n) {
             
             if(sum >= target) {
                 
                 end--;
-                
-                cout << start << " " << end << endl;
-
                 res = min(res, end - start + 1);
                 
                 sum -= a[start];
@@ -38,15 +30,10 @@ public:
             }
         }
         
-        // cout << start << " " << end << endl;
-        cout << res << endl;
         end--;
         
         while(sum >= target) {
             
-            // end--;
-            
-            cout << start << " " << end << endl;
             res = min(res, end - start + 1);
             
             sum -= a[start];
