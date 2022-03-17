25dbdb18979bdc99b7aa53a53c599d485703913a
AMAN <aman25barnwal@gmail.com>
2022-03-17 21:26:16 +0530

    Added solution - LeetHub

---
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int search(string pat, string txt) {
+	    
+	    int res = 0;
+	    int n = txt.length();
+	    
+	    // Find count of occurences of anagrams of word pat in the text
+	    
+	    // Here, window size = pat.length()
+	    int k = pat.length();
+	    
+	    unordered_map<char, int> mp;               // 1st arg - character, 2nd arg - Freq of that character
+	   
+	    for(auto &x : pat)              mp[x]++;
+	    
+	    // Using Sliding Window technique
+	    
+	    int i = 0, j = 0;
+	    int count = mp.size();          // No.of distinct characters in pattern
+	    
+	   
+	    while(j < n) {
+	   
+	        // if txt[j] is found in map, then decrease the freq of txt[j] by 1 in map
+	        if(mp.find(txt[j]) != mp.end()) {
+	            
+	            mp[txt[j]]--;
+	            
+	            // If txt[j] don't occur in map anymore, then we have to decrease no.of distinct char by 1
+	            if(mp[txt[j]] == 0)                     count--;
+	        }
+	        
+	        if(j - i + 1 < k) {
+	            j++;
+	        }
+	        
+	        // if size of window is equal to k
+	        
+	        else if(j - i + 1 == k) {
+	            
+	            // If there is no remaining character which remains in txt substring and present in pat
+	            
+	            if(count == 0) {
+	                res++;
+	            }
+	            
+	            // We need to remove ith index from the next window
+	            
+	            // if txt[i] is found in txt, then increment the freq of txt[i] in map by 1
+	            if(mp.find(txt[i]) != mp.end()) {
+	                
+	                mp[txt[i]]++;
+	                
+	                if(mp[txt[i]] == 1)                 count++;
+	            }
+	            
+	            // Slide the window by 1 unit to its right
+	            
+	            i++;
+	            j++;
+	        }
+	    } 
+	    
+	    return res;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string pat, txt;
+        cin >> txt >> pat;
+        Solution ob;
+        auto ans = ob.search(pat, txt);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
