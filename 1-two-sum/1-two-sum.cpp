493731c13528e9a53c98c7c42070c7441ebc9eab
AMAN <aman25barnwal@gmail.com>
2022-09-08 11:24:31 +0530

    Time: 100 ms (41.27%), Space: 10.2 MB (75.17%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-   
+    
     vector<int> twoSum(vector<int>& v, int tar) {
-        
+    
         vector<int> res;
         int n = v.size();
         
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n-1; i++) {
             
-            int temp = tar - v[i];
-            auto it = find(v.begin(), v.end(), temp);
+            auto it = find(v.begin()+i+1, v.end(), tar - v[i]);
             
-            if(it != v.end() && it - v.begin() != i) {
+            if(it != v.end()) {
                 
+                int ind = it - v.begin();
+                
+                res.push_back(ind);
                 res.push_back(i);
-                res.push_back(it - v.begin());
                 
-                break;
+                return res;
             }
         }
         
