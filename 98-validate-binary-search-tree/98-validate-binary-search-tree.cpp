e30eb56a8757ea3e03e36af62ffa39e59a9f86bb
AMAN <aman25barnwal@gmail.com>
2022-09-13 17:30:56 +0530

    Time: 24 ms (30.98%), Space: 21.9 MB (15.64%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -14,7 +14,9 @@ public:
     
     void inorder(TreeNode* root, vector<int> &v) {
         
-        if(root == NULL)                                    return;
+        if(root == NULL)                        return;
+        
+        // inorder means left -> root -> right
         
         inorder(root->left, v);
         v.push_back(root->val);
@@ -25,13 +27,13 @@ public:
     
     bool isValidBST(TreeNode* root) {
     
-        if(root == NULL)                                    return true;
+        if(root == NULL)                        return true;
         
-        vector<int> v;
+        vector<int> v; 
         inorder(root, v);
         
         for(int i=1; i<v.size(); i++) {
-            if(v[i-1] >= v[i])                               return false;
+            if(v[i-1] >= v[i])                  return false;
         }
         
         return true;
