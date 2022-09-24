9a5469963dc443ae5031325edd27be75efa4266b
AMAN <aman25barnwal@gmail.com>
2022-09-24 14:18:08 +0530

    Time: 1384 ms (5.03%), Space: 92.2 MB (15.70%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,92 +1,88 @@
 class Solution {
 public:
     
-    int largestRectangleArea(vector<int>& h) {
+    int largestRectangleArea(vector<int>& a) {
     
-        int maxArea = INT_MIN;
-        
-        int n = h.size();
-        
-        // Find the index of Nearest Smallest Left anf Nearest Smallest Right for every index respectively
-        // then Width of rectangle = (nsr - nsl - 1), then area = a[i] * (w[i])
-        
-        
-        stack<pair<int, int>> w;
-        
+        int area = INT_MIN;
+        int n = a.size();
+
+        // Find NGL and NGR at each index, and width = ngr - ngl - 1
+
+        stack<pair<int, int>> w;                // 1st element : value, 2nd element : index
         vector<int> nsl, nsr;
-        
-        // For index of nearest smallest left
+
+        // Finding Next Smaller Left index
+
         for(int i=0; i<n; i++) {
-            
-            if(w.empty() == true)                                       nsl.push_back(-1);
-            else if(w.top().first < h[i])                               nsl.push_back(w.top().second);
-            else if(w.top().first >= h[i]) {
-                
-                // Pop all elements greater or equal to h[i]
-                while(w.empty() == false && w.top().first >= h[i])      w.pop();
-                
-                // The top of stack contains the 1st element smaller than h[i]
-                
-                if(w.empty() == true)                                   nsl.push_back(-1);
-                else                                                    nsl.push_back(w.top().second);
+
+            if(w.empty() == true)                       nsl.push_back(-1);
+            else if(w.top().first < a[i])               nsl.push_back(w.top().second);
+            else if(w.top().first >= a[i]) {
+
+                while(w.empty() == false && w.top().first >= a[i])        w.pop();
+
+                if(w.empty() == true)                   nsl.push_back(-1);
+                else                                    nsl.push_back(w.top().second);
             }
-            
-            w.push({h[i], i});
+
+            w.push({a[i], i});
         }
-        
-        while(w.empty() == false)                                       w.pop();
-        
-         
-        // For index of nearest smallest right
+
+        for(auto &x : nsl)                              cout << x << " ";
+        // cout << endl;
+
+        while(w.empty() == false)                       w.pop();
+
+        // Finding Next Smaller Right
         for(int i=n-1; i>=0; i--) {
-            
-            if(w.empty() == true)                                     nsr.push_back(-1);
-            else if(w.top().first < h[i])                             nsr.push_back(w.top().second);
-            else if(w.top().first >= h[i]) {
-                
-                 // Pop all elements greater or equal to h[i]
-                while(w.empty() == false && w.top().first >= h[i])    w.pop();
-                
-                // The top of stack contains the 1st element smaller than h[i]
-                
-                if(w.empty() == true)                                 nsr.push_back(-1);
-                else                                                  nsr.push_back(w.top().second);
+
+            if(w.empty() == true)                       nsr.push_back(-1);
+            else if(w.top().first < a[i])               nsr.push_back(w.top().second);
+            else if(w.top().first >= a[i]) {
+
+                while(w.empty() == false && w.top().first >= a[i])        w.pop();
+
+                if(w.empty() == true)                   nsr.push_back(-1);
+                else                                    nsr.push_back(w.top().second);
             }
-            
-            w.push({h[i], i});
+
+            w.push({a[i], i});
         }
-        
+
         reverse(nsr.begin(), nsr.end());
-        
-//         for(auto &x : nsl)          cout << x << " ";
-//         cout << endl;
-        
-//         for(auto &x : nsr)          cout << x << " ";
-//         cout << endl;
-        
+
+        // for(auto &x : nsr)                              cout << x << " ";
+        // cout << endl;
+
+        int temp;
+        int width = INT_MIN;
+
+        // For each index, find the width
         for(int i=0; i<n; i++) {
+
+            if(nsl[i] == -1 && nsr[i] == -1) {
+                    temp = n;
+            }   
             
-            int temp_width;
+            // No NSL, i.e from [0, i-1], and from [i, nsr[i]-1]
+            else if(nsl[i] == -1) {
+                temp = i + (nsr[i] - 1 - i + 1);                // nsr[i] 
+            }
             
-            if(nsr[i] != -1 && nsl[i] != -1)                    temp_width = nsr[i] - nsl[i] - 1;
-            
+            // No NSR, i.e [nsl[i]+1, i], and from [i+1, n-1]
             else if(nsr[i] == -1) {
-                // No NSR i.e from ( (nsl[i] + 1) to i ) and from ( (i + 1) to (n - 1) )
-                temp_width = (n - 1 - i - 1 + 1) + (i - nsl[i] - 1 + 1);
+                temp = i - (nsl[i] + 1) + 1 + (n-1) - (i+1) + 1;        
             }
             
-            else if(nsl[i] == -1)  {
-                // No nsl i.e from 0 to (i - 1) and i to (nsr - 1)
-                temp_width = (i - 1 + 1) + (nsr[i] - 1 - i + 1);
-            }
-            
-            else                                                temp_width = n;
-            
-            maxArea = max(maxArea, temp_width * h[i]);
+            else {
+                temp = nsr[i]- nsl[i] - 1;                 
+            }   
+
+            area = max(area, temp * a[i]);
+            // cout << temp << " ";
         }
         
-        
-        return maxArea;
+        return area;    
     }
     
 };
