5e3b9a17f45662876d6b67efafa92efa6a757d79
AMAN <aman25barnwal@gmail.com>
2022-10-05 16:11:27 +0530

    Time: 596 ms (14.16%), Space: 18.6 MB (34.60%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -21,7 +21,7 @@ public:
         unordered_map<int, vector<int>> adj;
         
         for(int i=0; i<n-1; i++) {
-            for(int j=0; j<n; j++) {
+            for(int j=i+1; j<n; j++) {
                 
                 if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
                     adj[i].push_back(j);
