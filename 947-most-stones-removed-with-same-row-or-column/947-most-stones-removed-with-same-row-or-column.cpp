6d750194dc2429a7db61287c47de7d45c5cef553
AMAN <aman25barnwal@gmail.com>
2022-10-05 16:11:17 +0530

    Time: 858 ms (5.04%), Space: 21.1 MB (24.18%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    void dfs(int node, unordered_map<int, vector<int>> &adj, vector<int> &vis) {
+        
+        vis[node] = 1;
+        
+        for(auto &ch : adj[node]) {
+            if(vis[ch] == 0) {
+                dfs(ch, adj, vis);
+            }
+        }
+        
+        return;
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+    
+        int n = stones.size();
+        
+        unordered_map<int, vector<int>> adj;
+        
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n; j++) {
+                
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        
+        vector<int> vis(n, 0);
+        int cc = 0;
+        
+        // Let's consider a stone to be node
+        for(int i=0; i<n; i++) {
+            if(vis[i] == 0) {
+                dfs(i, adj, vis);
+                cc++;
+            }
+        }
+        
+        return n - cc;
+    }
+    
+};
