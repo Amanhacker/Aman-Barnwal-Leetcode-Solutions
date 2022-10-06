d6b2ac859110d9de49f4e0d5ab6b8329946d0fdd
AMAN <aman25barnwal@gmail.com>
2022-10-06 23:49:45 +0530

    Time: 833 ms (18.45%), Space: 60.8 MB (74.14%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    bool dfs(int node, int dest, unordered_map<int, vector<int>> &adj, vector<int> &vis) {
+        
+        if(node == dest)                                    return true;
+        
+        vis[node] = 1;
+        
+        for(auto &ch : adj[node]) {
+            if(vis[ch] == 0) {
+                if(dfs(ch, dest, adj, vis) == true)         return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prereq, vector<vector<int>>& q) {
+    
+        vector<bool> res;
+        unordered_map<int, vector<int>> adj;
+        
+        for(auto &x : prereq) {
+            adj[x[0]].push_back(x[1]);
+        }
+        
+        if(prereq.size() == 0) {
+            res.resize(n, false);
+            return res;
+        }
+        
+        vector<int> vis(n, 0);
+        
+        for(int i=0; i<q.size(); i++) {
+            
+            int u = q[i][0];
+            int v = q[i][1];
+            
+            // Make all nodes unvisited
+            fill(vis.begin(), vis.end(), 0);
+            res.push_back(dfs(u, v, adj, vis));
+        }
+        
+        return res;
+    }
+    
+};
