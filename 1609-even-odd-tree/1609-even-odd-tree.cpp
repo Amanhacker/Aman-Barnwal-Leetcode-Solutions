7a27de21a3743f379da2e45049e8c7f716b03b8b
AMAN <aman25barnwal@gmail.com>
2022-10-11 17:41:43 +0530

    Time: 612 ms (23.88%), Space: 151.2 MB (51.56%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -12,87 +12,69 @@
 class Solution {
 public:
     
-    void bfs(TreeNode* root, bool &flag) {
+    bool bfs(TreeNode* root) {
         
-        if(root == NULL || flag == false)                return;
+        if(root == NULL)                            return true;
+
+        bool flag = true;
         
         queue<TreeNode*> q;
         q.push(root);
         
+        vector<int> path;
         int lev = 0;
         
         while(q.empty() == false) {
             
             int n = q.size();
-            vector<int> path;
             
-            for(int i=0; i<n; i++) {
+            while(n--) {
                 
                 TreeNode* temp = q.front();
                 q.pop();
-
+                
                 path.push_back(temp->val);
                 
-                if(temp->left != NULL)                  q.push(temp->left);
-                if(temp->right != NULL)                 q.push(temp->right);
-            }
-
-            if(lev % 2 == 0) {
-                
-                // It should be strictly increasing from left to right, and should be odd
-                if(path[0] % 2 == 0) {
-                    flag = false;
-                    return;
-                }
-                
-                for(int i=1; i<path.size(); i++) {
-                
-                    if(path[i] % 2 == 0 || path[i-1] >= path[i]) {
-                        flag = false;
-                        return;
-                    }
-                }
+                if(temp->left != NULL)              q.push(temp->left);
+                if(temp->right != NULL)             q.push(temp->right);
             }
             
-            else {
+            // For odd-indexed level -> All even values -> All nodes strictly decreasing 
+            if(lev % 2 == 1) {
                 
-                // It should be strictly decreasing from left to right, and should be even
-                
-                if(path[0] % 2 == 1) {
-                    flag = false;
-                    return;
-                }
+                if(path[0] % 2 == 1)                    return false;
                 
                 for(int i=1; i<path.size(); i++) {
                     
-                    if(path[i] % 2 == 1 || path[i-1] <= path[i]) {
-                        flag = false;
-                        return;
-                    }
+                    if(path[i] % 2 == 1)                return false;
+                    if(path[i-1] <= path[i])            return false;
+                }
+            }
+            
+            // For even-indexed level -> All odd values -> All nodes strictly increasing 
+            else {
+                
+                if(path[0] % 2 == 0)                    return false;
+                
+                for(int i=1; i<path.size(); i++) {
+                    
+                    if(path[i] % 2 == 0)                return false;
+                    if(path[i-1] >= path[i])            return false;
                 }
             }
             
             lev++;
+            path.clear();
         }
         
-        return;
+        return true;
     }
     
     bool isEvenOddTree(TreeNode* root) {
     
-        if(root == NULL)                                return false;
+        if(root == NULL)                                return true;
         
-        // For even- indexed level, nodes are odd in strictly increasing from left to right
-        // For odd- indexed level,  nodes are odd in strictly decreasing from left to right
-        
-        // Considering 0-based level indexing
-        
-        if(root->val % 2 == 0)                          return false;
-        
-        bool flag = true;
-        bfs(root, flag);
-        
-        return flag;
+        return bfs(root);
     }
     
 };
