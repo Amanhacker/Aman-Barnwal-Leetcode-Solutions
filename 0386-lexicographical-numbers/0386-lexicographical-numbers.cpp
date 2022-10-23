5445ff0f9a392bd78af461c9900b15544fb5b819
AMAN <aman25barnwal@gmail.com>
2022-10-23 12:49:30 +0530

    Time: 26 ms (46.30%), Space: 11.4 MB (61.53%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     
+    void dfs(int i, int n, vector<int> &res) {
+        
+        if(i > n)                       return;
+        
+        res.push_back(i);
+        
+        for(int j=0; j<=9; j++) {
+            dfs(10 * i + j, n, res);    
+        }
+        
+        return;
+    }
+    
     vector<int> lexicalOrder(int n) {
     
         vector<int> res;
-        vector<string> v;
+
+        for(int i=1; i<=9; i++) {
+            dfs(i, n, res);
+        }
         
-        for(int i=1; i<=n; i++)                 v.push_back(to_string(i));
-        sort(v.begin(), v.end());
-        
-        for(auto &x : v)                        res.push_back(stoi(x));
         return res;
     }
     
