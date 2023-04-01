a999829d0828149bf7ac958eb06de1229b2a18cf
AMAN <aman25barnwal@gmail.com>
2023-04-01 22:49:35 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -10,31 +10,24 @@ using namespace std;
 class Solution{   
 public:
     
+    bool isSubsetSum(vector<int> a, int sum){
     
-    int knapsackHelper(vector<int> a, int n, int target) {
-    
-        // Using DP
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
-    
-        // Base Condition
-        for(int i=0; i<=n; i++)                     dp[i][0] = 1;
-        for(int j=1; j<=target; j++)                dp[0][j] = 0;
-    
+        int n = a.size();
+        
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        
+        for(int i=0; i<=n; i++)                             dp[i][0] = 1;
+        for(int j=1; j<=sum; j++)                           dp[0][j] = 0;
+        
         for(int i=1; i<=n; i++) {
-            for(int j=1; j<=target; j++) {
-    
-                if(a[i-1] <= j)                     dp[i][j] = max(dp[i-1][j-a[i-1]], dp[i-1][j]);
-                else                                dp[i][j] = dp[i-1][j];
+            for(int j=1; j<=sum; j++) {
+                if(a[i-1] <= j)                             dp[i][j] = max(dp[i-1][j-a[i-1]], dp[i-1][j]);
+                else                                        dp[i][j] = dp[i-1][j];
             }
         }
-    
-        return dp[n][target];
-    }
-    
-    bool isSubsetSum(vector<int> a, int target){
         
-        int n = a.size();
-        return knapsackHelper(a, n, target);
+        
+        return dp[n][sum];
     }
     
 };
