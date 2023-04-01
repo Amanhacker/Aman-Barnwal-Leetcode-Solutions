c2d36c2f54e735bc43e4863d3d4a7ef0e7f0a42e
AMAN <aman25barnwal@gmail.com>
2023-04-01 22:52:59 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,39 +9,29 @@ using namespace std;
 
 class Solution{
 public:
-   
-        
-    int knapsack(int a[], int n, int target) {
     
-        // Using DP
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
-    
-    
-        // Base Condition
-        for(int i=0; i<=n; i++)                     dp[i][0] = 1;
-        for(int j=1; j<=target; j++)                dp[0][j] = 0;
-    
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=target; j++) {
-    
-                if(a[i-1] <= j)                     dp[i][j] = max(dp[i-1][j-a[i-1]], dp[i-1][j]);
-                else                                dp[i][j] = dp[i-1][j];
-            }
-        }
-    
-    
-        return dp[n][target];
-    }
-
     int equalPartition(int n, int a[]) {
         
         int sum = 0;
+        for(int i=0; i<n; i++)                    sum += a[i];
         
-        for(int i=0; i<n; i++)                      sum += a[i];
-
-        if(sum % 2 != 0)                            return 0;
+        if(sum % 2 == 1)                    return 0;
         
-        return knapsack(a, n, sum / 2);
+        sum = sum / 2;
+        
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        
+        for(int i=0; i<=n; i++)             dp[i][0] = 1;
+        for(int j=1; j<=sum; j++)           dp[0][j] = 0;
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=sum; j++) {
+                if(a[i-1] <= j)             dp[i][j] = max(dp[i-1][j-a[i-1]], dp[i-1][j]);
+                else                        dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return dp[n][sum];       
     }
     
 };
