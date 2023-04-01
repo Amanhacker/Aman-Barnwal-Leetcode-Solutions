d1e46ad2e1b824bed2a63fbf46293c9347daddd1
AMAN <aman25barnwal@gmail.com>
2023-04-02 00:32:25 +0530

    Added solution - LeetHub

--- Target Sum - GFG/target-sum.cpp
+++ Target Sum - GFG/target-sum.cpp
@@ -36,12 +36,9 @@ class Solution {
         vector<vector<int>> dp(n + 1, vector<int>(s + 1, 0));
         
         for(int i=0; i<=n; i++)                 dp[i][0] = 1;
-        //for(int j=1; j<=s; j++)                 dp[0][j] = 0;
         
         for(int i=1; i<=n; i++) {
             for(int j=0; j<=s; j++) {
-                
-                //if(a[i-1] == 0)                 dp[i][j] = dp[i-1][j];
                 if(a[i-1] <= j)                 dp[i][j] = dp[i-1][j-a[i-1]] + dp[i-1][j];
                 else if(a[i-1] > j)             dp[i][j] = dp[i-1][j];
             }
